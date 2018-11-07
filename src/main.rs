45a63661658d11447da3dd21fc54d251af1f4971
Ryan Banks <rbanks@bitwise.io>
2018-11-07 17:11:50 -0600

    Remove duplate sp subcommand
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- src/main.rs
+++ src/main.rs
@@ -131,31 +131,6 @@ fn run() -> Result<(), error::CliError> {
                 (@arg output: --output -o +takes_value "File name to write payload to.")
             )
         )
-        (@subcommand sp => 
-          (about: "Create, update or delete smart permissions")
-          (@arg url: -U --url +takes_value "URL to the Sawtooth REST API")
-          (@arg wait: --wait +takes_value "A time in seconds to wait for batches to be committed")
-          (@subcommand create =>
-                (@arg org_id: +required "Organization ID ")
-                (@arg name: +required "Name of the Smart Permission")
-                (@arg filename: -f --filename +required +takes_value "Path to smart_permission")
-                (@arg key: -k --key +takes_value "Signing key name")
-                (@arg output: --output -o +takes_value "File name to write payload to.")
-            )
-            (@subcommand update =>
-                (@arg org_id: +required "Organization IDs")
-                (@arg name: +required "Name of the Smart Permission")
-                (@arg filename: -f --filename +required +takes_value "Path to smart_permission")
-                (@arg key: -k --key +takes_value "Signing key name")
-                (@arg output: --output -o +takes_value "File name to write payload to.")
-            )
-            (@subcommand delete =>
-                (@arg org_id: +required "Organization IDs")
-                (@arg name: +required "Name of the Smart Permission")
-                (@arg key: -k --key +takes_value "Signing key name")
-                (@arg output: --output -o +takes_value "File name to write payload to.")
-            )
-        )
     ).get_matches();
 
     let (batch_link, mut wait) = if let Some(upload_matches) = matches.subcommand_matches("upload") {
