080ef614dc3e84a07a1ced1232280b89155247fc
Andrea Gunderson <agunde@bitwise.io>
2019-04-04 15:18:28 -0500

    Remove unused protobuf use from sabre cli
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- src/smart_permission.rs
+++ src/smart_permission.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use protobuf;
 use sawtooth_sdk::signing;
 use std::fs::File;
 use std::io::prelude::*;
