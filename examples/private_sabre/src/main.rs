ebe7363faeaee90742b2d2c9380ed62003001fb3
Peter Schwarz <pschwarz@bitwise.io>
2019-04-16 15:18:06 -0500

    Add CLI args for services
    Add the ClI Args for the service, and process the config based on the
    matches.
    
    Splits main from run, in order to log the possible errors that might
    occur during the initial setup/execution.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- examples/private_sabre/src/main.rs
+++ examples/private_sabre/src/main.rs
@@ -25,12 +25,32 @@ use crate::error::ServiceError;
 const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn main() -> Result<(), ServiceError> {
+fn main() {
+    if let Err(err) = run() {
+        error!("Unable to run Sabre Service: {}", err);
+    }
+}
+
+fn run() -> Result<(), ServiceError> {
     let matches = clap_app!(myapp =>
         (name: APP_NAME)
         (version: VERSION)
         (author: "Contributors to Splinter")
         (about: "Private Sabre Service for Splinter")
+        (@arg service_id: -N --("service-id") +takes_value +required
+         "the name of this service, as presented to the network")
+        (@arg circuit: -C --circuit +takes_value +required
+         "the name of the circuit to connect to")
+        (@arg verifier: -V --verifier +takes_value +required +multiple
+         "the name of a service that will validate transactions")
+        (@arg transport: --transport +takes_value
+         "transport type for sockets, either raw or tls")
+        (@arg ca_file: --("ca-file") +takes_value
+         "file path to the trusted ca cert")
+        (@arg client_key: --("client-key") +takes_value
+         "file path for the TLS key used to connect to a splinterd node")
+        (@arg client_cert: --("client-cert") +takes_value
+         "file path the cert used to connect to a splinterd node")
         (@arg verbose: -v +multiple "Log verbosely")
     )
     .get_matches();
@@ -42,7 +62,33 @@ fn main() -> Result<(), ServiceError> {
     };
     simple_logger::init_with_level(log_level)?;
 
-    info!("Started Private Sabre Service");
+    let service_config = config::ConfigBuilder::default()
+        .with_cli_args(&matches)
+        .build()?;
+
+    info!(
+        "Started Private Sabre Service ({}/{})",
+        service_config.circuit(),
+        service_config.service_id()
+    );
+    info!("Binding on {}", service_config.bind());
+    info!(
+        "Connecting to network via {} over {}",
+        service_config.connect(),
+        match service_config.transport_config() {
+            &config::TransportConfig::Raw => "raw socket",
+            &config::TransportConfig::TLS { .. } => "TLS",
+        }
+    );
+    info!(
+        "Transactions verified by [{}]",
+        service_config
+            .verifiers()
+            .iter()
+            .map(|v| format!("{}/{}", service_config.circuit(), v))
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
 
     Ok(())
 }
