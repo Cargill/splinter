ec944acb4bb18974c981fc68b91b409ff5d87308
Ryan Banks <rbanks@bitwise.io>
2020-04-01 12:49:49 -0500

    Add brief descriptions of biome endpoints to doc comments
    Signed-off-by: Ryan Banks <rbanks@bitwise.io>

--- libsplinter/src/biome/rest_api/mod.rs
+++ libsplinter/src/biome/rest_api/mod.rs
@@ -107,7 +107,28 @@ use super::credentials::store::diesel::SplinterCredentialsStore;
 #[cfg(feature = "json-web-tokens")]
 use crate::rest_api::sessions::AccessTokenIssuer;
 
-/// Manages Biome REST API endpoints
+/// Provides the REST API endpoints for biome
+///
+/// The following endponts are provided
+///
+/// * `GET /biome/users/keys` - Get all keys for authorized user
+/// * `POST /biome/users/keys` - Create a new key for authorized user
+/// * `PATCH /biome/users/keys` - Update the display name associated with a key for
+///    an authorized user.
+/// * `GET /biome/users/keys/{public_key}` - Retrieve a key for an authroized user that has
+///    `public_key`
+/// * `DELETE /biome/users/keys/{public_key}` - delete a  key for an authorized user that has
+///    `public key`
+/// * `POST /biome/login` - Login enpoint for getting access tokens and refresh tokens
+/// * `PATCH /biome/login` - Login endpoint for removing refresh tokens
+/// * `POST /biome/register - Creates credentials for a user
+/// * `POST /biome/token` - Creates a new access token for the authorized user
+/// * `POST /biome/verify` - Verify a users password
+/// * `POST /biome/users` - Create new user
+/// * `GET /biome/user` - Get a list of all users in biome
+/// * `PUT /biome/user/{id}` - Update user with specified ID
+/// * `GET /biome/user/{id}` - Retrieve user with specified ID
+/// * `DELETE /biome/user/{id}` - Remove user with specified ID
 pub struct BiomeRestResourceManager {
     // Disable lint warning, for now this is only used if the biome-credentials feature is enabled
     #[allow(dead_code)]
