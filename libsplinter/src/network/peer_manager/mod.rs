f3fdc3afbb352e41e86ea00a7b98f3066c560da7
Andrea Gunderson <agunde@bitwise.io>
2020-04-08 13:54:12 -0500

    Fix typo in PeerManager::ShutdownHandler warn log
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- libsplinter/src/network/peer_manager/mod.rs
+++ libsplinter/src/network/peer_manager/mod.rs
@@ -260,7 +260,7 @@ pub struct ShutdownHandle {
 impl ShutdownHandle {
     pub fn shutdown(&self) {
         if self.sender.send(PeerManagerMessage::Shutdown).is_err() {
-            warn!("Connection manager is no longer running");
+            warn!("PeerManager is no longer running");
         }
     }
 }
