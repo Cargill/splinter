4ab16f702d777bdeea0aad0d1ac5a2482112835e
Logan Seeley <seeley@bitwise.io>
2020-04-27 13:04:48 -0500

    Add tests for the scabbard client
    Adds tests for all core scabbard client functionality.
    
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- libsplinter/src/service/scabbard/client/mod.rs
+++ libsplinter/src/service/scabbard/client/mod.rs
@@ -382,7 +382,7 @@ impl ServiceId {
 }
 
 /// Represents an entry in a Scabbard service's state.
-#[derive(Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct StateEntry {
     address: String,
     value: Vec<u8>,
@@ -401,7 +401,7 @@ impl StateEntry {
 }
 
 /// Used for deserializing the batch link provided by the Scabbard REST API.
-#[derive(Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Link {
     link: String,
 }
@@ -413,7 +413,7 @@ impl std::fmt::Display for Link {
 }
 
 /// Used for deserializing error responses from the Scabbard REST API.
-#[derive(Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct ErrorResponse {
     message: String,
 }
@@ -424,21 +424,426 @@ impl std::fmt::Display for ErrorResponse {
     }
 }
 
-#[cfg(test)]
+#[cfg(all(feature = "rest-api", feature = "rest-api-actix", test))]
 mod tests {
     use super::*;
+
+    #[cfg(feature = "scabbard-get-state")]
+    use std::collections::HashMap;
+    use std::sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    };
+    use std::time::SystemTime;
+
+    #[cfg(feature = "scabbard-get-state")]
+    use actix_web::web;
+    use actix_web::HttpResponse;
+    use futures::future::IntoFuture;
+
+    use crate::protocol;
+    use crate::rest_api::{
+        Method, ProtocolVersionRangeGuard, Resource, RestApiBuilder, RestApiServerError,
+        RestApiShutdownHandle,
+    };
+
+    const MOCK_CIRCUIT_ID: &str = "01234-abcde";
+    const MOCK_SERVICE_ID: &str = "ABCD";
+    const MOCK_BATCH_ID: &str = "batch_id";
 
     /// Verify that a `ServiceId` can be correctly parsed from a fully-qualified service ID string.
     #[test]
     fn service_id_from_string() {
         assert!(ServiceId::from_string("").is_err());
-        assert!(ServiceId::from_string("circuit").is_err());
+        assert!(ServiceId::from_string("01234-abcde").is_err());
         assert!(ServiceId::from_string("::").is_err());
-        assert!(ServiceId::from_string("circuit::").is_err());
-        assert!(ServiceId::from_string("::service_id").is_err());
+        assert!(ServiceId::from_string("01234-abcde::").is_err());
+        assert!(ServiceId::from_string("::ABCD").is_err());
 
-        let service_id = ServiceId::from_string("circuit::service_id").expect("failed to parse");
-        assert_eq!(service_id.circuit(), "circuit");
-        assert_eq!(service_id.service_id(), "service_id");
+        let service_id = ServiceId::from_string("01234-abcde::ABCD").expect("failed to parse");
+        assert_eq!(service_id.circuit(), "01234-abcde");
+        assert_eq!(service_id.service_id(), "ABCD");
+    }
+
+    /// Verify the `ScabbardClient::submit` method works properly.
+    #[test]
+    fn submit() {
+        let mut resource_manager = ResourceManager::new();
+        let (shutdown_handle, join_handle, bind_url) =
+            run_rest_api_on_open_port(resource_manager.resources());
+
+        let client = ScabbardClient::new(&format!("http://{}", bind_url));
+        let service_id = ServiceId::new(MOCK_CIRCUIT_ID, MOCK_SERVICE_ID);
+
+        // Verify that a basic batch submission with no wait time is successful
+        client
+            .submit(&service_id, vec![], None)
+            .expect("Failed to submit batches without wait");
+
+        // Verify that basic batch submision with a wait time is successful
+        client
+            .submit(&service_id, vec![], Some(Duration::from_secs(1)))
+            .expect("Failed to submit batches with wait");
+
+        // Verify that an invalid URL results in an error being returned
+        assert!(ScabbardClient::new("not a valid URL")
+            .submit(&service_id, vec![], None,)
+            .is_err());
+
+        // Verify that an error response code results in an error being returned
+        resource_manager.internal_server_error(true);
+        assert!(client.submit(&service_id, vec![], None,).is_err());
+        resource_manager.internal_server_error(false);
+
+        // Verify that an invalid batch results in an error being returned when `wait` is requested
+        resource_manager.invalid_batch(true);
+        assert!(client
+            .submit(&service_id, vec![], Some(Duration::from_secs(1)),)
+            .is_err());
+        resource_manager.invalid_batch(false);
+
+        // Verify that a batch not getting committed before the `wait` time elapses results in an
+        // error being returned
+        resource_manager.dont_commit(true);
+        assert!(client
+            .submit(&service_id, vec![], Some(Duration::from_secs(1)),)
+            .is_err());
+        resource_manager.dont_commit(false);
+
+        shutdown_handle
+            .shutdown()
+            .expect("unable to shutdown rest api");
+        join_handle.join().expect("Unable to join rest api thread");
+    }
+
+    /// Verify that the `ScabbardClient::get_state_at_address` method works properly.
+    #[cfg(feature = "scabbard-get-state")]
+    #[test]
+    fn get_state_at_address() {
+        let mut resource_manager = ResourceManager::new();
+        let (shutdown_handle, join_handle, bind_url) =
+            run_rest_api_on_open_port(resource_manager.resources());
+
+        let client = ScabbardClient::new(&format!("http://{}", bind_url));
+        let service_id = ServiceId::new(MOCK_CIRCUIT_ID, MOCK_SERVICE_ID);
+
+        // Verify that a request for an existing entry is successful and returns the right value
+        let value = client
+            .get_state_at_address(&service_id, &mock_state_entry().address)
+            .expect("Failed to get state for existing entry");
+        assert_eq!(value, Some(mock_state_entry().value));
+
+        // Verify that a request for a non-existent entry is successful and returns `None`
+        let value = client
+            .get_state_at_address(&service_id, "012345")
+            .expect("Failed to get state for non-existent entry");
+        assert_eq!(value, None);
+
+        // Verify that an invalid URL results in an error being returned
+        assert!(ScabbardClient::new("not a valid URL")
+            .get_state_at_address(&service_id, &mock_state_entry().address)
+            .is_err());
+
+        // Verify that an invalid address results in an error being returned
+        assert!(client
+            .get_state_at_address(&service_id, "not a valid address")
+            .is_err());
+
+        // Verify that an error response code results in an error being returned
+        resource_manager.internal_server_error(true);
+        assert!(client
+            .get_state_at_address(&service_id, &mock_state_entry().address)
+            .is_err());
+        resource_manager.internal_server_error(false);
+
+        shutdown_handle
+            .shutdown()
+            .expect("unable to shutdown rest api");
+        join_handle.join().expect("Unable to join rest api thread");
+    }
+
+    /// Verify that the `ScabbardClient::get_state_with_prefix` method works properly.
+    #[cfg(feature = "scabbard-get-state")]
+    #[test]
+    fn get_state_with_prefix() {
+        let mut resource_manager = ResourceManager::new();
+        let (shutdown_handle, join_handle, bind_url) =
+            run_rest_api_on_open_port(resource_manager.resources());
+
+        let client = ScabbardClient::new(&format!("http://{}", bind_url));
+        let service_id = ServiceId::new(MOCK_CIRCUIT_ID, MOCK_SERVICE_ID);
+
+        // Verify that a request with no prefix is successful and returns the right value
+        let entries = client
+            .get_state_with_prefix(&service_id, None)
+            .expect("Failed to get all entries");
+        assert_eq!(entries, vec![mock_state_entry()]);
+
+        // Verify that a request with a prefix that contains an existing entry is successful and
+        // returns the right value
+        let entries = client
+            .get_state_with_prefix(&service_id, Some(&mock_state_entry().address[..2]))
+            .expect("Failed to get entries under prefix with existing entry");
+        assert_eq!(entries, vec![mock_state_entry()]);
+
+        // Verify that a request with a prefix that does not contain any existing entries is
+        // successful and returns the right value
+        let entries = client
+            .get_state_with_prefix(&service_id, Some("01"))
+            .expect("Failed to get entries under prefix with existing entry");
+        assert_eq!(entries, vec![]);
+
+        // Verify that an invalid URL results in an error being returned
+        assert!(ScabbardClient::new("not a valid URL")
+            .get_state_with_prefix(&service_id, None)
+            .is_err());
+
+        // Verify that an invalid address prefix results in an error being returned
+        assert!(client
+            .get_state_with_prefix(&service_id, Some("not a valid address"))
+            .is_err());
+
+        // Verify that an error response code results in an error being returned
+        resource_manager.internal_server_error(true);
+        assert!(client.get_state_with_prefix(&service_id, None).is_err());
+        resource_manager.internal_server_error(false);
+
+        shutdown_handle
+            .shutdown()
+            .expect("unable to shutdown rest api");
+        join_handle.join().expect("Unable to join rest api thread");
+    }
+
+    struct ResourceManager {
+        resources: Vec<Resource>,
+        internal_server_error: Arc<AtomicBool>,
+        invalid_batch: Arc<AtomicBool>,
+        dont_commit: Arc<AtomicBool>,
+    }
+
+    impl ResourceManager {
+        fn new() -> Self {
+            let scabbard_base = format!("/scabbard/{}/{}", MOCK_CIRCUIT_ID, MOCK_SERVICE_ID);
+
+            let internal_server_error = Arc::new(AtomicBool::new(false));
+            let invalid_batch = Arc::new(AtomicBool::new(false));
+            let dont_commit = Arc::new(AtomicBool::new(false));
+
+            let mut resources = vec![];
+
+            let scabbard_base_clone = scabbard_base.clone();
+            let internal_server_error_clone = internal_server_error.clone();
+            let batches = Resource::build(&format!("{}/batches", scabbard_base))
+                .add_request_guard(ProtocolVersionRangeGuard::new(
+                    protocol::SCABBARD_ADD_BATCHES_PROTOCOL_MIN,
+                    protocol::SCABBARD_PROTOCOL_VERSION,
+                ))
+                .add_method(Method::Post, move |_, _| {
+                    if internal_server_error_clone.load(Ordering::SeqCst) {
+                        let response = ErrorResponse {
+                            message: "Request failed".into(),
+                        };
+                        Box::new(
+                            HttpResponse::InternalServerError()
+                                .json(response)
+                                .into_future(),
+                        )
+                    } else {
+                        let link = Link {
+                            link: format!(
+                                "{}/batch_statuses?ids={}",
+                                scabbard_base_clone, MOCK_BATCH_ID
+                            ),
+                        };
+                        Box::new(HttpResponse::Accepted().json(link).into_future())
+                    }
+                });
+            resources.push(batches);
+
+            let internal_server_error_clone = internal_server_error.clone();
+            let invalid_batch_clone = invalid_batch.clone();
+            let dont_commit_clone = dont_commit.clone();
+            let batch_statuses = Resource::build(&format!("{}/batch_statuses", scabbard_base))
+                .add_request_guard(ProtocolVersionRangeGuard::new(
+                    protocol::SCABBARD_BATCH_STATUSES_PROTOCOL_MIN,
+                    protocol::SCABBARD_PROTOCOL_VERSION,
+                ))
+                .add_method(Method::Get, move |_, _| {
+                    if internal_server_error_clone.load(Ordering::SeqCst) {
+                        let response = ErrorResponse {
+                            message: "Request failed".into(),
+                        };
+                        Box::new(
+                            HttpResponse::InternalServerError()
+                                .json(response)
+                                .into_future(),
+                        )
+                    } else if invalid_batch_clone.load(Ordering::SeqCst) {
+                        Box::new(
+                            HttpResponse::Ok()
+                                .json(vec![BatchInfo {
+                                    id: MOCK_BATCH_ID.into(),
+                                    status: BatchStatus::Invalid(vec![]),
+                                    timestamp: SystemTime::now(),
+                                }])
+                                .into_future(),
+                        )
+                    } else if dont_commit_clone.load(Ordering::SeqCst) {
+                        Box::new(
+                            HttpResponse::Ok()
+                                .json(vec![BatchInfo {
+                                    id: MOCK_BATCH_ID.into(),
+                                    status: BatchStatus::Pending,
+                                    timestamp: SystemTime::now(),
+                                }])
+                                .into_future(),
+                        )
+                    } else {
+                        Box::new(
+                            HttpResponse::Ok()
+                                .json(vec![BatchInfo {
+                                    id: MOCK_BATCH_ID.into(),
+                                    status: BatchStatus::Committed(vec![]),
+                                    timestamp: SystemTime::now(),
+                                }])
+                                .into_future(),
+                        )
+                    }
+                });
+            resources.push(batch_statuses);
+
+            #[cfg(feature = "scabbard-get-state")]
+            {
+                let internal_server_error_clone = internal_server_error.clone();
+                let state_address =
+                    Resource::build(&format!("{}/state/{{address}}", scabbard_base))
+                        .add_request_guard(ProtocolVersionRangeGuard::new(
+                            protocol::SCABBARD_GET_STATE_PROTOCOL_MIN,
+                            protocol::SCABBARD_PROTOCOL_VERSION,
+                        ))
+                        .add_method(Method::Get, move |request, _| {
+                            let address = request
+                                .match_info()
+                                .get("address")
+                                .expect("address should not be none");
+
+                            if internal_server_error_clone.load(Ordering::SeqCst) {
+                                let response = ErrorResponse {
+                                    message: "Request failed".into(),
+                                };
+                                Box::new(
+                                    HttpResponse::InternalServerError()
+                                        .json(response)
+                                        .into_future(),
+                                )
+                            } else if address == mock_state_entry().address {
+                                Box::new(
+                                    HttpResponse::Ok()
+                                        .json(mock_state_entry().value)
+                                        .into_future(),
+                                )
+                            } else {
+                                let response = ErrorResponse {
+                                    message: "Not found".into(),
+                                };
+                                Box::new(HttpResponse::NotFound().json(response).into_future())
+                            }
+                        });
+                resources.push(state_address);
+            }
+
+            #[cfg(feature = "scabbard-get-state")]
+            {
+                let internal_server_error_clone = internal_server_error.clone();
+                let state = Resource::build(&format!("{}/state", scabbard_base))
+                    .add_request_guard(ProtocolVersionRangeGuard::new(
+                        protocol::SCABBARD_LIST_STATE_PROTOCOL_MIN,
+                        protocol::SCABBARD_PROTOCOL_VERSION,
+                    ))
+                    .add_method(Method::Get, move |request, _| {
+                        let query: web::Query<HashMap<String, String>> =
+                            web::Query::from_query(request.query_string())
+                                .expect("Failed to get query string");
+                        let prefix = query.get("prefix").map(String::as_str);
+
+                        if internal_server_error_clone.load(Ordering::SeqCst) {
+                            let response = ErrorResponse {
+                                message: "Request failed".into(),
+                            };
+                            Box::new(
+                                HttpResponse::InternalServerError()
+                                    .json(response)
+                                    .into_future(),
+                            )
+                        } else {
+                            let return_entry = match prefix {
+                                Some(prefix) => mock_state_entry().address.starts_with(prefix),
+                                None => true,
+                            };
+                            let entries = if return_entry {
+                                vec![mock_state_entry()]
+                            } else {
+                                vec![]
+                            };
+                            Box::new(HttpResponse::Ok().json(entries).into_future())
+                        }
+                    });
+                resources.push(state);
+            }
+
+            Self {
+                resources,
+                internal_server_error,
+                invalid_batch,
+                dont_commit,
+            }
+        }
+
+        fn resources(&self) -> Vec<Resource> {
+            self.resources.clone()
+        }
+
+        fn internal_server_error(&mut self, val: bool) {
+            self.internal_server_error.store(val, Ordering::SeqCst);
+        }
+
+        fn invalid_batch(&mut self, val: bool) {
+            self.invalid_batch.store(val, Ordering::SeqCst);
+        }
+
+        fn dont_commit(&mut self, val: bool) {
+            self.dont_commit.store(val, Ordering::SeqCst);
+        }
+    }
+
+    #[cfg(feature = "scabbard-get-state")]
+    fn mock_state_entry() -> StateEntry {
+        StateEntry {
+            address: "abcdef".into(),
+            value: b"value".to_vec(),
+        }
+    }
+
+    fn run_rest_api_on_open_port(
+        resources: Vec<Resource>,
+    ) -> (RestApiShutdownHandle, std::thread::JoinHandle<()>, String) {
+        (10000..20000)
+            .find_map(|port| {
+                let bind_url = format!("127.0.0.1:{}", port);
+                let result = RestApiBuilder::new()
+                    .with_bind(&bind_url)
+                    .add_resources(resources.clone())
+                    .build()
+                    .expect("Failed to build REST API")
+                    .run();
+                match result {
+                    Ok((shutdown_handle, join_handle)) => {
+                        Some((shutdown_handle, join_handle, bind_url))
+                    }
+                    Err(RestApiServerError::BindError(_)) => None,
+                    Err(err) => panic!("Failed to run REST API: {}", err),
+                }
+            })
+            .expect("No port available")
     }
 }
