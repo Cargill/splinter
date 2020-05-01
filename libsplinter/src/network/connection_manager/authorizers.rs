c09b29ae0471249b05728f9f66724de3785ff834
Peter Schwarz <pschwarz@bitwise.io>
2020-05-01 17:43:37 -0500

    Add Authorizers
    Add the Authorizers struct, which provides the capability to map
    endpoint prefixes to authorizer instances.  This allows for multiple
    authorizers to be implemented for a given ConnectionManager instance.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/network/connection_manager/authorizers.rs
+++ libsplinter/src/network/connection_manager/authorizers.rs
@@ -76,6 +76,54 @@ impl Authorizer for InprocAuthorizer {
     }
 }
 
+/// A set of Authorizers.
+///
+/// Authorizers processes a connection by matching the remote endpoint of a connection against a
+/// list of prefixes.  If it finds a match, it calls the authorizer configured for that match.
+///
+/// These prefixes are processed in the order they are provided.  A default can be configured with
+/// the empty string as a prefix, but should be added last.
+#[derive(Default)]
+pub struct Authorizers {
+    authorizers: Vec<(String, Box<dyn Authorizer>)>,
+}
+
+impl Authorizers {
+    /// Construct a new Authorizers set.
+    pub fn new() -> Self {
+        Authorizers::default()
+    }
+
+    /// Add an Authorizer instances that will match on the given prefix.
+    ///
+    /// Connections are evaluated against these prefixes based on the order they are added.
+    pub fn add_authorizer(&mut self, match_prefix: &str, authorizer: impl Authorizer + 'static) {
+        self.authorizers
+            .push((match_prefix.to_string(), Box::new(authorizer)));
+    }
+}
+
+impl Authorizer for Authorizers {
+    fn authorize_connection(
+        &self,
+        connection_id: String,
+        connection: Box<dyn Connection>,
+        on_complete: AuthorizerCallback,
+    ) -> Result<(), AuthorizerError> {
+        for (match_prefix, authorizer) in &self.authorizers {
+            if connection.remote_endpoint().starts_with(match_prefix) {
+                return authorizer.authorize_connection(connection_id, connection, on_complete);
+            }
+        }
+
+        Err(AuthorizerError(format!(
+            "no authorizer found for {} ({})",
+            connection_id,
+            connection.remote_endpoint()
+        )))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -136,6 +184,106 @@ mod tests {
         }
     }
 
+    // Test that the Authorizers struct will correctly route the connection to the appropriate
+    // authorizer.
+    //
+    // Test that:
+    // 1. setup an authorizer with three match prefixes
+    // 2. test that connections that match each prefix should trigger the correct auhtorizer
+    // 3. Check that non-matching connections sttll faill in this case, but falling through to the
+    //    default.
+    #[test]
+    fn authorizers_configured_authorizations() {
+        let inproc_authorizer = InprocAuthorizer::new(vec![(
+            "inproc://test-conn".to_string(),
+            "test-ident1".to_string(),
+        )]);
+
+        let future_inproc_authorizer = NoopAuthorizer::new("test-ident2");
+
+        let default_authorizer = InprocAuthorizer::new(vec![(
+            "protocol://other-conn".to_string(),
+            "test-ident3".to_string(),
+        )]);
+
+        let mut authorizers = Authorizers::new();
+        authorizers.add_authorizer("inproc2", future_inproc_authorizer);
+        authorizers.add_authorizer("inproc", inproc_authorizer);
+        authorizers.add_authorizer("", default_authorizer);
+
+        let (tx, rx) = mpsc::channel();
+
+        let tx1 = tx.clone();
+        authorizers
+            .authorize_connection(
+                "abcd-1234".into(),
+                Box::new(MockConnection::new("inproc://test-conn")),
+                Box::new(move |result| tx1.send(result).map_err(Box::from)),
+            )
+            .unwrap();
+
+        let result = rx.recv().unwrap();
+
+        match result {
+            AuthorizationResult::Authorized { identity, .. } => {
+                assert_eq!("test-ident1", &identity)
+            }
+            AuthorizationResult::Unauthorized { .. } => panic!("should have been authorized"),
+        }
+
+        let tx2 = tx.clone();
+        authorizers
+            .authorize_connection(
+                "abcd-1234".into(),
+                Box::new(MockConnection::new("inproc2://test-conn")),
+                Box::new(move |result| tx2.send(result).map_err(Box::from)),
+            )
+            .unwrap();
+
+        let result = rx.recv().unwrap();
+
+        match result {
+            AuthorizationResult::Authorized { identity, .. } => {
+                assert_eq!("test-ident2", &identity)
+            }
+            AuthorizationResult::Unauthorized { .. } => panic!("should have been authorized"),
+        }
+
+        let tx3 = tx.clone();
+        authorizers
+            .authorize_connection(
+                "abcd-1234".into(),
+                Box::new(MockConnection::new("protocol://other-conn")),
+                Box::new(move |result| tx3.send(result).map_err(Box::from)),
+            )
+            .unwrap();
+
+        let result = rx.recv().unwrap();
+
+        match result {
+            AuthorizationResult::Authorized { identity, .. } => {
+                assert_eq!("test-ident3", &identity)
+            }
+            AuthorizationResult::Unauthorized { .. } => panic!("should have been authorized"),
+        }
+
+        let tx4 = tx.clone();
+        authorizers
+            .authorize_connection(
+                "abcd-1234".into(),
+                Box::new(MockConnection::new("tcp://some-tcp:4444")),
+                Box::new(move |result| tx4.send(result).map_err(Box::from)),
+            )
+            .unwrap();
+
+        let result = rx.recv().unwrap();
+
+        match result {
+            AuthorizationResult::Authorized { .. } => panic!("should not have been authorized"),
+            AuthorizationResult::Unauthorized { .. } => (),
+        }
+    }
+
     struct MockConnection {
         remote_endpoint: String,
     }
@@ -171,6 +319,34 @@ mod tests {
 
         fn evented(&self) -> &dyn mio::Evented {
             unimplemented!()
+        }
+    }
+
+    struct NoopAuthorizer {
+        authorized_id: String,
+    }
+
+    impl NoopAuthorizer {
+        fn new(id: &str) -> Self {
+            Self {
+                authorized_id: id.to_string(),
+            }
+        }
+    }
+
+    impl Authorizer for NoopAuthorizer {
+        fn authorize_connection(
+            &self,
+            connection_id: String,
+            connection: Box<dyn Connection>,
+            callback: AuthorizerCallback,
+        ) -> Result<(), AuthorizerError> {
+            (*callback)(AuthorizationResult::Authorized {
+                connection_id,
+                connection,
+                identity: self.authorized_id.clone(),
+            })
+            .map_err(|err| AuthorizerError(format!("Unable to return result: {}", err)))
         }
     }
 }
