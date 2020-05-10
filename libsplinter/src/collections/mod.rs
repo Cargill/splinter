a228e3ec9b0920eab4075dccf61a7dbdfed9b0b3
Shawn T. Amundson <amundson@bitwise.io>
2020-05-10 12:26:42 -0500

    Add module documentation to crate::collections
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- libsplinter/src/collections/mod.rs
+++ libsplinter/src/collections/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Collections used within this crate.
+
 mod bi_hash_map;
 mod ref_map;
 
