ffa23c473c81b5e423d57cd5b472b7359953014f
Peter Schwarz <pschwarz@bitwise.io>
2020-05-14 17:32:23 -0500

    Add the circuit::component module
    This module will provide trait implementations that can enabled the use
    of service networking handlers.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/circuit/mod.rs
+++ libsplinter/src/circuit/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "service-network")]
+pub mod component;
 pub mod directory;
 pub mod handlers;
 pub mod service;
