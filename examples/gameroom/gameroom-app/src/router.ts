eb856a752ddf4db2d3f0a665f50412b21daf4274
Shannyn Telander <telander@bitwise.io>
2020-05-18 08:45:05 -0500

    Add a server error page
    Adds a page to handle a server error, 5XX response status, and to
    accurately display the type of error encountered to the user.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- examples/gameroom/gameroom-app/src/router.ts
+++ examples/gameroom/gameroom-app/src/router.ts
@@ -94,6 +94,11 @@ const router = new Router({
           name: 'not-found',
           component: () => import('@/views/NotFound.vue'),
         },
+        {
+          path: '/server-error',
+          name: 'server-error',
+          component: () => import('@/views/ServerError.vue'),
+        },
       ],
     },
   ],
