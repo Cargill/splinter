c9c88932e2be8f197d14cfd49b8ee23fb5dfb277
Peter Schwarz <pschwarz@bitwise.io>
2020-05-19 14:11:19 -0500

    Add the splinter::peer::component module
    This module will hold trait implementations in order to support the
    message sending facilities of service components.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/peer/mod.rs
+++ libsplinter/src/peer/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "service-network")]
+pub mod component;
 mod connector;
 mod error;
 pub mod interconnect;
