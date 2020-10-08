2048b756e077c110aee9d8387a6ef717951e693d
Peter Schwarz <pschwarz@bitwise.io>
2020-10-08 15:56:24 -0500

    Add UserTokens struct
    The user tokens struct will hold the token information for a given
    OAuth2 login.  It includes an optional refresh token, as it is not
    always guaranteed that a provider will return one.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- cli/src/action/login/oauth/mod.rs
+++ cli/src/action/login/oauth/mod.rs
@@ -11,3 +11,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+/// Contains the user information returned by an OAuth2 Provider.
+pub struct UserTokens {
+    pub provider_type: String,
+    pub access_token: String,
+    pub refresh_token: Option<String>,
+}
+
+impl std::fmt::Debug for UserTokens {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("UserTokens")
+            .field("provider_type", &self.provider_type)
+            .field("access_token", &"<Redacted>".to_string())
+            .field(
+                "refresh_token",
+                &self.refresh_token.as_deref().map(|_| "<Redacted>"),
+            )
+            .finish()
+    }
+}
