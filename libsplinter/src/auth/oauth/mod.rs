9a6d29c04cb34e85a33fa8b3c663dda8238e0cf2
Logan Seeley <seeley@bitwise.io>
2020-10-09 11:51:59 -0500

    Add new `oauth` module to libsplinter
    Adds the `splinter::auth::oauth` module behind the experimental `oauth`
    feature. This module will contain the code that supports OAuth2
    authentication in Splinter.
    
    Signed-off-by: Logan Seeley <seeley@bitwise.io>

--- libsplinter/src/auth/oauth/mod.rs
+++ libsplinter/src/auth/oauth/mod.rs
@@ -12,7 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Authentication and authorization for Splinter
-
-#[cfg(feature = "oauth")]
-pub mod oauth;
+//! Support for OAuth2 authorization in Splinter
