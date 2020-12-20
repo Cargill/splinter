f34f61818cc7fd14718beae0b30b12e05c1f93ea
Shannyn Telander <telander@bitwise.io>
2020-12-20 09:23:51 -0600

    Add AdminServiceEventStore database migrations
    Adds migration folders for both SQLite and PostgreSQL backend types.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

---
+++ libsplinter/src/migrations/diesel/postgres/migrations/2020-12-22-125000_admin_service_event_store/down.sql
@@ -0,0 +1,23 @@
+--- Copyright 2018-2020 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS admin_service_event;
+DROP TABLE IF EXISTS admin_event_circuit_proposal;
+DROP TABLE IF EXISTS admin_event_proposed_circuit;
+DROP TABLE IF EXISTS admin_event_proposed_node;
+DROP TABLE IF EXISTS admin_event_proposed_node_endpoint;
+DROP TABLE IF EXISTS admin_event_proposed_service;
+DROP TABLE IF EXISTS admin_event_proposed_service_argument;
+DROP TABLE IF EXISTS admin_event_vote_record;
