16ca88660afe56987053735fa5f6d30b8b194161
Shannyn Telander <telander@bitwise.io>
2020-12-21 10:15:11 -0600

    Add traits to implement event store database
    Adds the necessary traits to implement the operations for the
    AdminServiceEventStore, as well as the initial structure for the
    operations module. The traits added includes the
    `DieselAdminServiceEventStore` and the `AdminServiceEventStoreOperations`
    traits.
    
    The Clone implementations for the `DieselAdminServiceEventStore` are
    also added, behind an `AdminServiceEventStore`-specific experimental
    feature representing the type of database used, PostgreSQL or SQLite.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- libsplinter/src/admin/service/event/store/diesel/mod.rs
+++ libsplinter/src/admin/service/event/store/diesel/mod.rs
@@ -14,6 +14,98 @@
 
 //! Database backend support for the `AdminServiceEventStore`, powered by
 //! [`Diesel`](https://crates.io/crates/diesel).
+//!
+//! This module contains the [`DieselAdminServiceStore`].
+//!
+//! [`DieselAdminServiceEventStore`]: struct.DieselAdminServiceEventStore.html
+//! [`AdminServiceEventStore`]: ../trait.AdminServiceEventStore.html
 
 mod models;
+mod operations;
 mod schema;
+
+use diesel::r2d2::{ConnectionManager, Pool};
+
+use crate::admin::service::event::{
+    store::{AdminServiceEventStore, AdminServiceEventStoreError, EventIter},
+    AdminServiceEvent,
+};
+use crate::admin::service::messages;
+
+/// A database-backed AdminServiceEventStore, powered by [`Diesel`](https://crates.io/crates/diesel).
+pub struct DieselAdminServiceEventStore<C: diesel::Connection + 'static> {
+    connection_pool: Pool<ConnectionManager<C>>,
+}
+
+impl<C: diesel::Connection> DieselAdminServiceEventStore<C> {
+    /// Creates a new `DieselAdminServiceEventStore`.
+    ///
+    /// # Arguments
+    ///
+    ///  * `connection_pool`: connection pool for the database
+    pub fn _new(connection_pool: Pool<ConnectionManager<C>>) -> Self {
+        DieselAdminServiceEventStore { connection_pool }
+    }
+}
+
+#[cfg(feature = "sqlite")]
+impl Clone for DieselAdminServiceEventStore<diesel::sqlite::SqliteConnection> {
+    fn clone(&self) -> Self {
+        Self {
+            connection_pool: self.connection_pool.clone(),
+        }
+    }
+}
+
+#[cfg(feature = "sqlite")]
+impl AdminServiceEventStore for DieselAdminServiceEventStore<diesel::sqlite::SqliteConnection> {
+    fn add_event(
+        &self,
+        _event: messages::AdminServiceEvent,
+    ) -> Result<AdminServiceEvent, AdminServiceEventStoreError> {
+        unimplemented!()
+    }
+
+    fn list_events_since(&self, _start: i64) -> Result<EventIter, AdminServiceEventStoreError> {
+        unimplemented!()
+    }
+
+    fn list_events_by_management_type_since(
+        &self,
+        _management_type: String,
+        _start: i64,
+    ) -> Result<EventIter, AdminServiceEventStoreError> {
+        unimplemented!()
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl Clone for DieselAdminServiceEventStore<diesel::pg::PgConnection> {
+    fn clone(&self) -> Self {
+        Self {
+            connection_pool: self.connection_pool.clone(),
+        }
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl AdminServiceEventStore for DieselAdminServiceEventStore<diesel::pg::PgConnection> {
+    fn add_event(
+        &self,
+        _event: messages::AdminServiceEvent,
+    ) -> Result<AdminServiceEvent, AdminServiceEventStoreError> {
+        unimplemented!()
+    }
+
+    fn list_events_since(&self, _start: i64) -> Result<EventIter, AdminServiceEventStoreError> {
+        unimplemented!()
+    }
+
+    fn list_events_by_management_type_since(
+        &self,
+        _management_type: String,
+        _start: i64,
+    ) -> Result<EventIter, AdminServiceEventStoreError> {
+        unimplemented!()
+    }
+}
