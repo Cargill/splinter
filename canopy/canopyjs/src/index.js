42670e339688d28daee970fa4b9744f8a50131a8
Ryan Beck-Buysse <rbuysse@bitwise.io>
2021-01-14 19:02:39 -0600

    Update copyright headers to include 2021
    Signed-off-by: Ryan Beck-Buysse <rbuysse@bitwise.io>

--- canopy/canopyjs/src/index.js
+++ canopy/canopyjs/src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /**
- * Copyright 2018-2020 Cargill Incorporated
+ * Copyright 2018-2021 Cargill Incorporated
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
