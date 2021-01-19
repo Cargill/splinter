1dafe441db47a22926c7985d00d880601aa82cec
Isabel Tomb <tomb@bitwise.io>
2021-01-19 10:37:12 -0600

    Update error message in scabbard cli
    Update error messages in the scabbard cli 'create_cylinder_jwt_auth'
    method to join search paths with ':'
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

--- services/scabbard/cli/src/key.rs
+++ services/scabbard/cli/src/key.rs
@@ -111,9 +111,18 @@ pub fn create_cylinder_jwt_auth(key_name: Option<&str>) -> Result<String, CliErr
             let path = &current_user_search_path();
             load_key(key_name, path)
                 .map_err(|err| CliError::action_error(&err.to_string()))?
-                .ok_or_else(|| CliError::action_error( {
-                    &format!("No signing key found in {:?}.  Either specify the --key argument or generate the default key via splinter keygen", path)
-                }))?
+                .ok_or_else(|| {
+                    CliError::action_error({
+                        &format!(
+                            "No signing key found in {}. Either specify the --key argument or \
+                            generate the default key via splinter keygen",
+                            path.iter()
+                                .map(|path| path.as_path().display().to_string())
+                                .collect::<Vec<String>>()
+                                .join(":")
+                        )
+                    })
+                })?
         }
     } else {
         let path = match env::var("CYLINDER_PATH") {
@@ -132,9 +141,18 @@ pub fn create_cylinder_jwt_auth(key_name: Option<&str>) -> Result<String, CliErr
         };
         load_key(&current_user_key_name(), &path)
             .map_err(|err| CliError::action_error(&err.to_string()))?
-            .ok_or_else(|| CliError::action_error({
-                &format!("No signing key found in {:?}.  Either specify the --key argument or generate the default key via splinter keygen", path)
-            }))?
+            .ok_or_else(|| {
+                CliError::action_error({
+                    &format!(
+                        "No signing key found in {}. Either specify the --key argument or \
+                        generate the default key via splinter keygen",
+                        path.iter()
+                            .map(|path| path.as_path().display().to_string())
+                            .collect::<Vec<String>>()
+                            .join(":")
+                    )
+                })
+            })?
     };
 
     let context = Secp256k1Context::new();
