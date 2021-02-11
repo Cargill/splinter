8a2a8c25f9062f8e19972bac094e5ef6717274a7
Shawn T. Amundson <amundson@bitwise.io>
2021-02-11 11:13:19 -0600

    Add initial Actix Web 3 framework support
    This adds the start of pieces we need to easily standup and manage the
    REST API without any wrapper code (such as the approach taken with the
    Actix Web 1 support). This includes support for a RestApi struct and
    its lifecycle (RestApiBuilder -> RunnbleRestApi -> RestApi -> shutdown).
    
    This is only an initial cut at the core pieces; much remains like
    support for authorization, etc.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

---
+++ libsplinter/src/rest_api/actix_web_3/runnable.rs
@@ -0,0 +1,55 @@
+// Copyright 2018-2021 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Contains the implementation of `RunnableRestApi`.
+
+use crate::rest_api::{RestApiBind, RestApiServerError};
+
+use super::{ResourceProvider, RestApi};
+
+/// A configured REST API which may best started with `run` function.
+pub struct RunnableRestApi {
+    pub(super) resource_providers: Vec<Box<dyn ResourceProvider>>,
+    pub(super) bind: RestApiBind,
+}
+
+impl RunnableRestApi {
+    /// Start the REST API and finish any necessary setup such as binding to ports, adding resource
+    /// endpoints, etc.
+    pub fn run(self) -> Result<RestApi, RestApiServerError> {
+        let RunnableRestApi {
+            resource_providers,
+            bind,
+        } = self;
+
+        let (bind_url, acceptor_opt) = match bind {
+            RestApiBind::Secure {
+                bind,
+                cert_path,
+                key_path,
+            } => {
+                let mut acceptor =
+                    openssl::ssl::SslAcceptor::mozilla_modern(openssl::ssl::SslMethod::tls())?;
+                acceptor.set_private_key_file(key_path, openssl::ssl::SslFiletype::PEM)?;
+                acceptor.set_certificate_chain_file(&cert_path)?;
+                acceptor.check_private_key()?;
+
+                (bind, Some(acceptor))
+            }
+            RestApiBind::Insecure(url) => (url, None),
+        };
+
+        Ok(RestApi::new(bind_url, acceptor_opt, resource_providers)?)
+    }
+}
