96b9fc3e1af2297a613d60c96de470e2ec0d9c72
Shannyn Telander <telander@bitwise.io>
2021-03-22 11:57:10 -0500

    Add helper function to create a node's service ID
    This function takes a reference to a node and a `circuit_id` used to
    create the `ServiceId` struct needed to pass into the `Scabbard` client's
    `submit` function. This function is at the top-level mod file as it is
    used by the `circuit_create` and `circuit_disband` tests, and will
    eventually be used by future `circuit_*` integration tests.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- splinterd/tests/admin/mod.rs
+++ splinterd/tests/admin/mod.rs
@@ -22,3 +22,29 @@ mod circuit_list;
 mod node_lifecycle;
 mod payload;
 mod registry;
+
+use scabbard::client::ServiceId;
+use splinterd::node::Node;
+
+// Helper function to generate the `ServiceId` for the provided Node on the circuit specified by
+// the `circuit_id` argument. The generic definition of this function allows for this function to
+// be used for any Node that is apart of the circuit specified.
+pub(super) fn get_node_service_id(circuit_id: &str, node: &Node) -> ServiceId {
+    // Retrieve the node's associated `service_id` from the circuit just committed
+    let circuit = node
+        .admin_service_client()
+        .fetch_circuit(&circuit_id)
+        .expect("Unable to fetch circuit")
+        .unwrap();
+    // Create the `ServiceId` struct based on the node's associated `service_id` and the
+    // committed `circuit_id`
+    let node_service = &circuit
+        .roster
+        .iter()
+        .find(|service_slice| &service_slice.node_id == node.node_id())
+        .expect("Circuit committed without service for node")
+        .service_id;
+    format!("{}::{}", &circuit_id, node_service)
+        .parse::<ServiceId>()
+        .expect("Unable to parse `ServiceId`")
+}
