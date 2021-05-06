7bf27568ea8e1c7fd26df05b3300bc4caa65183b
Andrea Gunderson <agunde@bitwise.io>
2021-05-06 11:18:31 -0500

    Add native protocols for new authorization messages
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- libsplinter/src/protocol/authorization.rs
+++ libsplinter/src/protocol/authorization.rs
@@ -29,6 +29,19 @@ pub enum AuthorizationMessage {
     AuthorizationError(AuthorizationError),
 
     TrustRequest(TrustRequest),
+
+    // v1 messages
+    AuthComplete(AuthComplete),
+    AuthProtocolRequest(AuthProtocolRequest),
+    AuthProtocolResponse(AuthProtocolResponse),
+
+    AuthTrustRequest(AuthTrustRequest),
+    AuthTrustResponse(AuthTrustResponse),
+
+    AuthChallengeNonceRequest(AuthChallengeNonceRequest),
+    AuthChallengeNonceResponse(AuthChallengeNonceResponse),
+    AuthChallengeSubmitRequest(AuthChallengeSubmitRequest),
+    AuthChallengeSubmitResponse(AuthChallengeSubmitResponse),
 }
 
 /// The possible types of authorization that may be computed during the handshake.
@@ -202,6 +215,256 @@ impl FromNative<AuthorizationError> for authorization::AuthorizationError {
     }
 }
 
+// ------------- v1 messages ----------
+
+/// A successful authorization message.
+///
+/// This message is returned after Trust or Challenge authorization is completed
+#[derive(Debug)]
+pub struct AuthComplete;
+
+/// A protocol request message.
+///
+/// This message provides supported protocol versions and requests that an agreed upon version is
+/// returned.
+#[derive(Debug)]
+pub struct AuthProtocolRequest {
+    auth_protocol_min: i32,
+    auth_protocol_max: i32,
+}
+
+#[derive(Debug)]
+pub enum PeerAuthorizationType {
+    Trust,
+    Challenge,
+}
+
+/// A protocol response message.
+///
+/// This message returns the agreed upon authorization protocol and a list of supported peer
+/// authorization types.
+#[derive(Debug)]
+pub struct AuthProtocolResponse {
+    auth_protocol: i32,
+    accepted_authorization_type: Vec<PeerAuthorizationType>,
+}
+
+/// A trust request.
+///
+/// A trust request is sent to a node, if the other node accepts trust authorization a trust
+/// response will be returned.
+#[derive(Debug)]
+pub struct AuthTrustRequest {
+    identity: String,
+}
+
+/// A successful trust authorization.
+///
+/// This message is returned if trust a request is accepted
+#[derive(Debug)]
+pub struct AuthTrustResponse;
+
+/// A challenge nounce request
+///
+/// This request is for a nonce that will be used to create the signature used in the
+/// AuthChallengeSubmitRequest message
+#[derive(Debug)]
+pub struct AuthChallengeNonceRequest;
+
+/// A challenge nounce response
+///
+/// This response contains nonce that must be used to create the signature used in the
+/// AuthChallengeSubmitRequest message
+#[derive(Debug)]
+pub struct AuthChallengeNonceResponse {
+    nonce: Vec<u8>,
+}
+
+/// A challenge submit request
+///
+/// This request contains the signature created from the nonce in the AuthChallengeNonceResponse
+/// and the public key for the signature.
+#[derive(Debug)]
+pub struct AuthChallengeSubmitRequest {
+    public_key: Vec<u8>,
+    signature: Vec<u8>,
+}
+
+/// A successful challenge authorization.
+///
+/// This message is returned if challenge submit request is accepted
+#[derive(Debug)]
+pub struct AuthChallengeSubmitResponse;
+
+impl FromProto<authorization::AuthComplete> for AuthComplete {
+    fn from_proto(_: authorization::AuthComplete) -> Result<Self, ProtoConversionError> {
+        Ok(AuthComplete)
+    }
+}
+
+impl FromNative<AuthComplete> for authorization::AuthComplete {
+    fn from_native(_: AuthComplete) -> Result<Self, ProtoConversionError> {
+        Ok(authorization::AuthComplete::new())
+    }
+}
+
+impl FromProto<authorization::AuthProtocolRequest> for AuthProtocolRequest {
+    fn from_proto(
+        source: authorization::AuthProtocolRequest,
+    ) -> Result<Self, ProtoConversionError> {
+        Ok(AuthProtocolRequest {
+            auth_protocol_min: source.get_auth_protocol_min(),
+            auth_protocol_max: source.get_auth_protocol_max(),
+        })
+    }
+}
+
+impl FromNative<AuthProtocolRequest> for authorization::AuthProtocolRequest {
+    fn from_native(req: AuthProtocolRequest) -> Result<Self, ProtoConversionError> {
+        let mut proto_request = authorization::AuthProtocolRequest::new();
+        proto_request.set_auth_protocol_min(req.auth_protocol_min);
+        proto_request.set_auth_protocol_max(req.auth_protocol_max);
+        Ok(proto_request)
+    }
+}
+
+impl FromProto<authorization::AuthProtocolResponse> for AuthProtocolResponse {
+    fn from_proto(
+        source: authorization::AuthProtocolResponse,
+    ) -> Result<Self, ProtoConversionError> {
+        use authorization::AuthProtocolResponse_PeerAuthorizationType::*;
+        Ok(AuthProtocolResponse {
+            auth_protocol: source.get_auth_protocol(),
+            accepted_authorization_type: source
+                .get_accepted_authorization_type()
+                .iter()
+                .map(|auth_type| match auth_type {
+                    UNSET_AUTHORIZATION_TYPE => Err(ProtoConversionError::InvalidTypeError(
+                        "No handshake mode was set".into(),
+                    )),
+                    TRUST => Ok(PeerAuthorizationType::Trust),
+                    CHALLENGE => Ok(PeerAuthorizationType::Challenge),
+                })
+                .collect::<Result<Vec<_>, ProtoConversionError>>()?,
+        })
+    }
+}
+
+impl FromNative<AuthProtocolResponse> for authorization::AuthProtocolResponse {
+    fn from_native(req: AuthProtocolResponse) -> Result<Self, ProtoConversionError> {
+        use authorization::AuthProtocolResponse_PeerAuthorizationType::*;
+
+        let mut proto_request = authorization::AuthProtocolResponse::new();
+        proto_request.set_auth_protocol(req.auth_protocol);
+        proto_request.set_accepted_authorization_type(
+            req.accepted_authorization_type
+                .iter()
+                .map(|auth_type| match auth_type {
+                    PeerAuthorizationType::Trust => TRUST,
+                    PeerAuthorizationType::Challenge => CHALLENGE,
+                })
+                .collect(),
+        );
+        Ok(proto_request)
+    }
+}
+
+impl FromProto<authorization::AuthTrustRequest> for AuthTrustRequest {
+    fn from_proto(
+        mut source: authorization::AuthTrustRequest,
+    ) -> Result<Self, ProtoConversionError> {
+        Ok(AuthTrustRequest {
+            identity: source.take_identity(),
+        })
+    }
+}
+
+impl FromNative<AuthTrustRequest> for authorization::AuthTrustRequest {
+    fn from_native(req: AuthTrustRequest) -> Result<Self, ProtoConversionError> {
+        let mut proto_request = authorization::AuthTrustRequest::new();
+        proto_request.set_identity(req.identity);
+        Ok(proto_request)
+    }
+}
+
+impl FromProto<authorization::AuthTrustResponse> for AuthTrustResponse {
+    fn from_proto(_: authorization::AuthTrustResponse) -> Result<Self, ProtoConversionError> {
+        Ok(AuthTrustResponse)
+    }
+}
+
+impl FromNative<AuthTrustResponse> for authorization::AuthTrustResponse {
+    fn from_native(_: AuthTrustResponse) -> Result<Self, ProtoConversionError> {
+        Ok(authorization::AuthTrustResponse::new())
+    }
+}
+
+impl FromProto<authorization::AuthChallengeNonceRequest> for AuthChallengeNonceRequest {
+    fn from_proto(
+        _: authorization::AuthChallengeNonceRequest,
+    ) -> Result<Self, ProtoConversionError> {
+        Ok(AuthChallengeNonceRequest)
+    }
+}
+
+impl FromNative<AuthChallengeNonceRequest> for authorization::AuthChallengeNonceRequest {
+    fn from_native(_: AuthChallengeNonceRequest) -> Result<Self, ProtoConversionError> {
+        Ok(authorization::AuthChallengeNonceRequest::new())
+    }
+}
+
+impl FromProto<authorization::AuthChallengeNonceResponse> for AuthChallengeNonceResponse {
+    fn from_proto(
+        mut source: authorization::AuthChallengeNonceResponse,
+    ) -> Result<Self, ProtoConversionError> {
+        Ok(AuthChallengeNonceResponse {
+            nonce: source.take_nonce(),
+        })
+    }
+}
+
+impl FromNative<AuthChallengeNonceResponse> for authorization::AuthChallengeNonceResponse {
+    fn from_native(req: AuthChallengeNonceResponse) -> Result<Self, ProtoConversionError> {
+        let mut proto_request = authorization::AuthChallengeNonceResponse::new();
+        proto_request.set_nonce(req.nonce);
+        Ok(proto_request)
+    }
+}
+
+impl FromProto<authorization::AuthChallengeSubmitRequest> for AuthChallengeSubmitRequest {
+    fn from_proto(
+        mut source: authorization::AuthChallengeSubmitRequest,
+    ) -> Result<Self, ProtoConversionError> {
+        Ok(AuthChallengeSubmitRequest {
+            public_key: source.take_public_key(),
+            signature: source.take_signature(),
+        })
+    }
+}
+
+impl FromNative<AuthChallengeSubmitRequest> for authorization::AuthChallengeSubmitRequest {
+    fn from_native(req: AuthChallengeSubmitRequest) -> Result<Self, ProtoConversionError> {
+        let mut proto_request = authorization::AuthChallengeSubmitRequest::new();
+        proto_request.set_public_key(req.public_key);
+        proto_request.set_signature(req.signature);
+        Ok(proto_request)
+    }
+}
+
+impl FromNative<AuthChallengeSubmitResponse> for authorization::AuthChallengeSubmitResponse {
+    fn from_native(_: AuthChallengeSubmitResponse) -> Result<Self, ProtoConversionError> {
+        Ok(authorization::AuthChallengeSubmitResponse::new())
+    }
+}
+
+impl FromProto<authorization::AuthChallengeSubmitResponse> for AuthChallengeSubmitResponse {
+    fn from_proto(
+        _: authorization::AuthChallengeSubmitResponse,
+    ) -> Result<Self, ProtoConversionError> {
+        Ok(AuthChallengeSubmitResponse)
+    }
+}
+
 impl FromProto<authorization::AuthorizationMessage> for AuthorizationMessage {
     fn from_proto(
         source: authorization::AuthorizationMessage,
@@ -233,6 +496,53 @@ impl FromProto<authorization::AuthorizationMessage> for AuthorizationMessage {
             >::from_bytes(
                 source.get_payload()
             )?)),
+            AUTH_COMPLETE => Ok(AuthorizationMessage::AuthComplete(FromBytes::<
+                authorization::AuthComplete,
+            >::from_bytes(
+                source.get_payload()
+            )?)),
+            AUTH_PROTOCOL_REQUEST => Ok(AuthorizationMessage::AuthProtocolRequest(FromBytes::<
+                authorization::AuthProtocolRequest,
+            >::from_bytes(
+                source.get_payload(),
+            )?)),
+            AUTH_PROTOCOL_RESPONSE => Ok(AuthorizationMessage::AuthProtocolResponse(FromBytes::<
+                authorization::AuthProtocolResponse,
+            >::from_bytes(
+                source.get_payload(),
+            )?)),
+            AUTH_TRUST_REQUEST => Ok(AuthorizationMessage::AuthTrustRequest(FromBytes::<
+                authorization::AuthTrustRequest,
+            >::from_bytes(
+                source.get_payload(),
+            )?)),
+            AUTH_TRUST_RESPONSE => Ok(AuthorizationMessage::AuthTrustResponse(FromBytes::<
+                authorization::AuthTrustResponse,
+            >::from_bytes(
+                source.get_payload(),
+            )?)),
+            AUTH_CHALLENGE_NONCE_REQUEST => Ok(AuthorizationMessage::AuthChallengeNonceRequest(
+                FromBytes::<authorization::AuthChallengeNonceRequest>::from_bytes(
+                    source.get_payload(),
+                )?,
+            )),
+            AUTH_CHALLENGE_NONCE_RESPONSE => Ok(AuthorizationMessage::AuthChallengeNonceResponse(
+                FromBytes::<authorization::AuthChallengeNonceResponse>::from_bytes(
+                    source.get_payload(),
+                )?,
+            )),
+            AUTH_CHALLENGE_SUBMIT_REQUEST => Ok(AuthorizationMessage::AuthChallengeSubmitRequest(
+                FromBytes::<authorization::AuthChallengeSubmitRequest>::from_bytes(
+                    source.get_payload(),
+                )?,
+            )),
+            AUTH_CHALLENGE_SUBMIT_RESPONSE => {
+                Ok(AuthorizationMessage::AuthChallengeSubmitResponse(
+                    FromBytes::<authorization::AuthChallengeSubmitResponse>::from_bytes(
+                        source.get_payload(),
+                    )?,
+                ))
+            }
             UNSET_AUTHORIZATION_MESSAGE_TYPE => Err(ProtoConversionError::InvalidTypeError(
                 "no message type was set".into(),
             )),
@@ -276,6 +586,60 @@ impl FromNative<AuthorizationMessage> for authorization::AuthorizationMessage {
                 message.set_payload(IntoBytes::<authorization::TrustRequest>::into_bytes(
                     payload,
                 )?);
+            }
+            AuthorizationMessage::AuthComplete(payload) => {
+                message.set_message_type(AUTH_COMPLETE);
+                message.set_payload(IntoBytes::<authorization::AuthComplete>::into_bytes(
+                    payload,
+                )?);
+            }
+            AuthorizationMessage::AuthProtocolRequest(payload) => {
+                message.set_message_type(AUTH_PROTOCOL_REQUEST);
+                message.set_payload(IntoBytes::<authorization::AuthProtocolRequest>::into_bytes(
+                    payload,
+                )?);
+            }
+            AuthorizationMessage::AuthProtocolResponse(payload) => {
+                message.set_message_type(AUTH_PROTOCOL_RESPONSE);
+                message.set_payload(
+                    IntoBytes::<authorization::AuthProtocolResponse>::into_bytes(payload)?,
+                );
+            }
+            AuthorizationMessage::AuthTrustRequest(payload) => {
+                message.set_message_type(AUTH_TRUST_REQUEST);
+                message.set_payload(IntoBytes::<authorization::AuthTrustRequest>::into_bytes(
+                    payload,
+                )?);
+            }
+            AuthorizationMessage::AuthTrustResponse(payload) => {
+                message.set_message_type(AUTH_TRUST_RESPONSE);
+                message.set_payload(IntoBytes::<authorization::AuthTrustResponse>::into_bytes(
+                    payload,
+                )?);
+            }
+            AuthorizationMessage::AuthChallengeNonceRequest(payload) => {
+                message.set_message_type(AUTH_CHALLENGE_NONCE_REQUEST);
+                message.set_payload(
+                    IntoBytes::<authorization::AuthChallengeNonceRequest>::into_bytes(payload)?,
+                );
+            }
+            AuthorizationMessage::AuthChallengeNonceResponse(payload) => {
+                message.set_message_type(AUTH_CHALLENGE_NONCE_RESPONSE);
+                message.set_payload(
+                    IntoBytes::<authorization::AuthChallengeNonceResponse>::into_bytes(payload)?,
+                );
+            }
+            AuthorizationMessage::AuthChallengeSubmitRequest(payload) => {
+                message.set_message_type(AUTH_CHALLENGE_SUBMIT_REQUEST);
+                message.set_payload(
+                    IntoBytes::<authorization::AuthChallengeSubmitRequest>::into_bytes(payload)?,
+                );
+            }
+            AuthorizationMessage::AuthChallengeSubmitResponse(payload) => {
+                message.set_message_type(AUTH_CHALLENGE_SUBMIT_RESPONSE);
+                message.set_payload(
+                    IntoBytes::<authorization::AuthChallengeSubmitResponse>::into_bytes(payload)?,
+                );
             }
         }
         Ok(message)
