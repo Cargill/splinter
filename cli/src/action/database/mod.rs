f461099bbc56f150be09bacd59a2c4f6418ca73f
Lee Bradley <bradley@bitwise.io>
2021-05-24 12:11:26 -0500

    Abstract splinter environment from sqlite
    Abstract functions to gather splinter environment information away from
    sqlite so that other database functionality can access it.
    
    Signed-off-by: Lee Bradley <bradley@bitwise.io>

--- cli/src/action/database/mod.rs
+++ cli/src/action/database/mod.rs
@@ -15,7 +15,12 @@
 #[cfg(feature = "sqlite")]
 mod sqlite;
 
+#[cfg(feature = "upgrade")]
+mod upgrade;
+
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
+use std::{env, fs};
 
 use clap::ArgMatches;
 use diesel::{connection::Connection as _, pg::PgConnection};
@@ -29,6 +34,9 @@ use self::sqlite::{get_default_database, sqlite_migrations};
 
 use super::Action;
 
+const SPLINTER_HOME_ENV: &str = "SPLINTER_HOME";
+const SPLINTER_STATE_DIR_ENV: &str = "SPLINTER_STATE_DIR";
+const DEFAULT_STATE_DIR: &str = "/var/lib/splinter";
 pub struct MigrateAction;
 
 impl Action for MigrateAction {
@@ -96,5 +104,103 @@ impl FromStr for ConnectionUri {
                 s
             ))),
         }
+    }
+}
+
+/// Represents the SplinterEnvironment data
+struct SplinterEnvironment {
+    state_dir: Option<String>,
+    home_dir: Option<String>,
+    default_dir: &'static str,
+}
+
+impl SplinterEnvironment {
+    pub fn load() -> Self {
+        SplinterEnvironment {
+            state_dir: env::var(SPLINTER_STATE_DIR_ENV).ok(),
+            home_dir: env::var(SPLINTER_HOME_ENV).ok(),
+            default_dir: DEFAULT_STATE_DIR,
+        }
+    }
+
+    fn try_canonicalize<P: Into<PathBuf>>(dir: P) -> PathBuf {
+        let dir: PathBuf = dir.into();
+        fs::canonicalize(dir.clone()).unwrap_or(dir)
+    }
+
+    /// Returns the path to the state directory
+    ///
+    /// If `SPLINTER_STATE_DIR` is set, returns `SPLINTER_STATE_DIR`.
+    /// If `SPLINTER_HOME` is set, returns `SPLINTER_HOME/data`.
+    /// Otherwise, returns the default directory `/var/lib/splinter`
+    pub fn get_state_path(&self) -> PathBuf {
+        if let Some(state_dir) = self.state_dir.as_ref() {
+            Self::try_canonicalize(PathBuf::from(&state_dir))
+        } else if let Some(home_dir) = self.home_dir.as_ref() {
+            Self::try_canonicalize(Path::new(&home_dir).join("data"))
+        } else {
+            Self::try_canonicalize(PathBuf::from(&self.default_dir))
+        }
+    }
+}
+
+#[cfg(test)]
+mod splinter_env_tests {
+    use super::*;
+
+    const TEST_STATE_DIR: &str = "/test/state/dir";
+    const TEST_HOME: &str = "/test/home";
+    const TEST_DEFAULT_DIR: &str = "/test/default";
+
+    #[test]
+    fn splinter_environment_get_state_path_with_state_dir_and_home_defaults_to_state() {
+        assert_eq!(
+            PathBuf::from(TEST_STATE_DIR),
+            SplinterEnvironment {
+                state_dir: Some(TEST_STATE_DIR.to_string()),
+                home_dir: Some(TEST_HOME.to_string()),
+                default_dir: TEST_DEFAULT_DIR,
+            }
+            .get_state_path()
+        );
+    }
+
+    #[test]
+    fn splinter_environment_get_state_path_with_home_without_state_dir_returns_home_plus_data() {
+        assert_eq!(
+            PathBuf::from(TEST_HOME.to_string() + "/data"),
+            SplinterEnvironment {
+                state_dir: None,
+                home_dir: Some(TEST_HOME.to_string()),
+                default_dir: TEST_DEFAULT_DIR,
+            }
+            .get_state_path()
+        );
+    }
+
+    #[test]
+    fn splinter_environment_get_state_path_with_only_state_dir_returns_state_dir() {
+        assert_eq!(
+            PathBuf::from(TEST_STATE_DIR),
+            SplinterEnvironment {
+                state_dir: Some(TEST_STATE_DIR.to_string()),
+                home_dir: None,
+                default_dir: TEST_DEFAULT_DIR,
+            }
+            .get_state_path()
+        );
+    }
+
+    #[test]
+    fn splinter_environment_get_state_path_with_nothing_returns_default() {
+        assert_eq!(
+            PathBuf::from(TEST_DEFAULT_DIR),
+            SplinterEnvironment {
+                state_dir: None,
+                home_dir: None,
+                default_dir: TEST_DEFAULT_DIR,
+            }
+            .get_state_path()
+        );
     }
 }
