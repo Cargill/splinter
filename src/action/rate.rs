e919c3ca974e84191b76d5cee1fa7a7c089a2bfc
Peter Schwarz <pschwarz@bitwise.io>
2021-06-17 15:17:20 -0500

    Fix wrong_self_convention lint
    This change fixes the wrong_self_convention lint introduced with the
    Rust 1.53.0 release. The lint warns of non-conventional relationships
    between naming and how self is taken.
    
    See
    https://rust-lang.github.io/rust-clippy/master/index.html#wrong_self_convention
    for more details.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- src/action/rate.rs
+++ src/action/rate.rs
@@ -134,7 +134,7 @@ impl std::str::FromStr for TimeUnit {
 }
 
 impl TimeUnit {
-    fn to_sec(&self) -> u64 {
+    fn to_sec(self) -> u64 {
         use TimeUnit::*;
         match self {
             Hour => 60 * 60,
