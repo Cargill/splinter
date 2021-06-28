a6a0f84837dc1f446be30203134ec8b508e4b1d6
Lee Bradley <bradley@bitwise.io>
2021-06-28 13:36:47 -0500

    Stabilize deprecate-yaml feature
    Signed-off-by: Lee Bradley <bradley@bitwise.io>

--- splinterd/src/daemon.rs
+++ splinterd/src/daemon.rs
@@ -16,12 +16,7 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
-#[cfg(feature = "deprecate-yaml")]
 use std::fs;
-#[cfg(any(
-    feature = "authorization-handler-allow-keys",
-    feature = "deprecate-yaml"
-))]
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{mpsc::channel, Arc};
@@ -167,40 +162,37 @@ impl SplinterDaemon {
 
         let store_factory = create_store_factory(&self.db_url)?;
 
-        #[cfg(feature = "deprecate-yaml")]
-        {
-            let circuits_location = Path::new(&self.state_dir).join("circuits.yaml");
-            let proposals_location = Path::new(&self.state_dir).join("circuit_proposals.yaml");
+        let circuits_location = Path::new(&self.state_dir).join("circuits.yaml");
+        let proposals_location = Path::new(&self.state_dir).join("circuit_proposals.yaml");
 
-            let circuits_location_exists = circuits_location.exists();
-            let proposals_location_exists = proposals_location.exists();
+        let circuits_location_exists = circuits_location.exists();
+        let proposals_location_exists = proposals_location.exists();
 
-            if circuits_location_exists || proposals_location_exists {
-                if circuits_location_exists {
-                    error!(
-                        "Found outdated circuit state file: {}",
-                        fs::canonicalize(&circuits_location)
-                            .unwrap_or(circuits_location)
-                            .to_string_lossy()
-                    );
-                }
-
-                if proposals_location_exists {
-                    error!(
-                        "Found outdated proposals state file: {}",
-                        fs::canonicalize(&proposals_location)
-                            .unwrap_or(proposals_location)
-                            .to_string_lossy()
-                    );
-                }
-
-                return Err(StartError::StorageError(format!(
-                    "Run the `splinter upgrade` command to update outdated state files to \
-                        a Splinter {}.{} database",
-                    env!("CARGO_PKG_VERSION_MAJOR", "unknown"),
-                    env!("CARGO_PKG_VERSION_MINOR", "unknown")
-                )));
+        if circuits_location_exists || proposals_location_exists {
+            if circuits_location_exists {
+                error!(
+                    "Found outdated circuit state file: {}",
+                    fs::canonicalize(&circuits_location)
+                        .unwrap_or(circuits_location)
+                        .to_string_lossy()
+                );
             }
+
+            if proposals_location_exists {
+                error!(
+                    "Found outdated proposals state file: {}",
+                    fs::canonicalize(&proposals_location)
+                        .unwrap_or(proposals_location)
+                        .to_string_lossy()
+                );
+            }
+
+            return Err(StartError::StorageError(format!(
+                "Run the `splinter upgrade` command to update outdated state files to \
+                        a Splinter {}.{} database",
+                env!("CARGO_PKG_VERSION_MAJOR", "unknown"),
+                env!("CARGO_PKG_VERSION_MINOR", "unknown")
+            )));
         }
 
         let table = RoutingTable::default();
