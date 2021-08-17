4cf83f3e4e5657d77a342883c942b209b4ba32b8
Caleb Hill <hill@bitwise.io>
2021-08-17 07:26:40 -0500

    Fix rustfmt issues
    Couple of small rustfmt formatting issues, completely non functional
    change.
    
    Signed-off-by: Caleb Hill <hill@bitwise.io>

--- examples/gameroom/daemon/src/rest_api/routes/submit.rs
+++ examples/gameroom/daemon/src/rest_api/routes/submit.rs
@@ -317,11 +317,12 @@ async fn check_batch_status(
 
                 // If batch status is still pending and the wait time has not yet passed,
                 // send request again to re-check the batch status
-                if batches_info
-                    .iter()
-                    .any(|batch_info|
-                         matches!(batch_info.status, BatchStatus::Pending | BatchStatus::Valid(_)))
-                    && Instant::now().duration_since(start_time) < Duration::from_secs(wait)
+                if batches_info.iter().any(|batch_info| {
+                    matches!(
+                        batch_info.status,
+                        BatchStatus::Pending | BatchStatus::Valid(_)
+                    )
+                }) && Instant::now().duration_since(start_time) < Duration::from_secs(wait)
                 {
                     // wait one second before sending request again
                     sleep(Duration::from_secs(1));
