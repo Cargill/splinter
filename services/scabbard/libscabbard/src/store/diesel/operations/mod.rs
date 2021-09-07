21eda26aa4ff12635b56e5b15e4df733a9561195
Peter Schwarz <pschwarz@bitwise.io>
2021-09-07 13:44:29 -0500

    Add get_current_commit_hash operation
    This change adds the operation to return the current commit hash, if it
    exists.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- services/scabbard/libscabbard/src/store/diesel/operations/mod.rs
+++ services/scabbard/libscabbard/src/store/diesel/operations/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub(super) mod get_current_commit_hash;
+
 pub struct CommitHashStoreOperations<'a, C> {
     conn: &'a C,
 }
