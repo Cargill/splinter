e1b723f80e24efa8783d0d88e8926c5b8595c471
Peter Schwarz <pschwarz@bitwise.io>
2021-09-07 13:43:31 -0500

    Add CommitHashStoreOperations
    This adds the root struct for diesel operations.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- services/scabbard/libscabbard/src/store/diesel/operations/mod.rs
+++ services/scabbard/libscabbard/src/store/diesel/operations/mod.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod models;
-mod operations;
-mod schema;
+pub struct CommitHashStoreOperations<'a, C> {
+    conn: &'a C,
+}
+
+impl<'a, C: diesel::Connection> CommitHashStoreOperations<'a, C> {
+    pub fn new(conn: &'a C) -> Self {
+        CommitHashStoreOperations { conn }
+    }
+}
