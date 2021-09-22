4ba4dfcf92090d948fbb0a66a70f957750b1db36
Caleb Hill <hill@bitwise.io>
2021-09-22 12:04:12 -0500

    Move node_id access logic and checks to module
    Moving the node_id access and selection logic to its own module.
    
    Signed-off-by: Caleb Hill <hill@bitwise.io>

---
+++ splinterd/src/node_id.rs
@@ -0,0 +1,78 @@
+// Copyright 2018-2021 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use rand::Rng;
+#[cfg(feature = "node-file-block")]
+use splinter::error::InternalError;
+#[cfg(feature = "node-file-block")]
+use splinter::node_id::store::NodeIdStore;
+
+use crate::error::UserError;
+
+pub fn get_node_id(
+    passed_in_node_id: Option<String>,
+    #[cfg(feature = "node-file-block")] node_id_store: Box<dyn NodeIdStore>,
+) -> Result<String, UserError> {
+    #[cfg(feature = "node-file-block")]
+    {
+        get_from_store(passed_in_node_id, node_id_store)
+    }
+    #[cfg(not(feature = "node-file-block"))]
+    {
+        // unwrap is safe here because the or_else always returns as Some
+        Ok(passed_in_node_id
+            .or_else(|| Some(get_random_node_id()))
+            .unwrap())
+    }
+}
+
+fn get_random_node_id() -> String {
+    format!("n{}", rand::thread_rng().gen::<u16>().to_string())
+}
+
+#[cfg(feature = "node-file-block")]
+fn get_from_store(
+    config_node_id: Option<String>,
+    node_id_store: Box<dyn NodeIdStore>,
+) -> Result<String, UserError> {
+    let save_new_node_id = |node_id| -> Result<(), UserError> {
+        node_id_store
+            .set_node_id(node_id)
+            .map_err(|err| UserError::from(InternalError::from_source(Box::new(err))))
+    };
+
+    match (node_id_store.get_node_id(), config_node_id) {
+        (Ok(Some(db_node_id)), Some(config_node_id)) => {
+            if db_node_id == config_node_id {
+                Ok(db_node_id)
+            } else {
+                Err(UserError::InvalidArgument(format!(
+                    "node_id from database {} does not match node_id from config {}",
+                    db_node_id, config_node_id
+                )))
+            }
+        }
+        (Ok(Some(db_node_id)), None) => Ok(db_node_id),
+        (Ok(None), Some(config_node_id)) => {
+            save_new_node_id(config_node_id.clone())?;
+            Ok(config_node_id)
+        }
+        (Ok(None), None) => {
+            let node_id = get_random_node_id();
+            save_new_node_id(node_id.clone())?;
+            Ok(node_id)
+        }
+        (Err(err), _) => Err(UserError::from(InternalError::from_source(Box::new(err)))),
+    }
+}
