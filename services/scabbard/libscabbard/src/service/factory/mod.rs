7f922bff9f50571869284ac46d9abe03b95411e7
Peter Schwarz <pschwarz@bitwise.io>
2021-10-06 16:18:23 -0500

    Add factory_database module
    This change adds the factory_database module which is enabled if the
    database-support feature is active.
    
    As of this commit, it's contents are the same.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- services/scabbard/libscabbard/src/service/factory/mod.rs
+++ services/scabbard/libscabbard/src/service/factory/mod.rs
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "database-support")]
+mod factory_database;
 #[cfg(not(feature = "database-support"))]
 mod factory_static_configuration;
 
+#[cfg(feature = "database-support")]
+pub use factory_database::*;
 #[cfg(not(feature = "database-support"))]
 pub use factory_static_configuration::*;
