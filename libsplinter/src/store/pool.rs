a34eed32b38363fb207a25f99e14b5e25e570ad3
Peter Schwarz <pschwarz@bitwise.io>
2021-12-17 17:19:59 -0600

    Add test for SQLite multithreaded read-write
    This change adds a test to verify that ConnectionPool<SqliteConnection>
    will properly handle multithreaded reads and writes.
    
    This test uses diesel, but creates its own table and uses raw queries,
    in order to stay agnostic to any schema changes that may occur in the
    future.
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- libsplinter/src/store/pool.rs
+++ libsplinter/src/store/pool.rs
@@ -85,3 +85,105 @@ impl<C: diesel::Connection> From<Arc<RwLock<Pool<ConnectionManager<C>>>>> for Co
         Self::WriteExclusive(pool)
     }
 }
+
+#[cfg(all(test, feature = "sqlite"))]
+mod tests {
+    use super::*;
+
+    use diesel::connection::SimpleConnection;
+    use diesel::prelude::*;
+
+    /// Given a SqliteConnection pool
+    /// 1. Create a simple table
+    /// 2. Wrap the diesel pool in the ConnectionPool with an Arc-RwLock combo
+    /// 2. Spawn N threads and pass a clone of the wrapped pool to each
+    /// 3. In each thread, perform M reads and writes; this helps increase the likelihood that
+    ///    there will be a write collision.
+    /// 4. Validate that all threads exit successfully.
+    #[test]
+    fn test_multithreaded_read_write() -> Result<(), Box<dyn std::error::Error>> {
+        let pool = create_connection_pool("test_multithreaded_read_write")?;
+
+        pool.get()?.batch_execute(
+            r#"
+            CREATE TABLE test_table (
+                id TEXT PRIMARY KEY,
+                value TEXT NOT NULL
+            );
+            "#,
+        )?;
+
+        let conn_pool = ConnectionPool::from(Arc::new(RwLock::new(pool)));
+
+        let (tx, rx) = std::sync::mpsc::channel();
+        let thread_count = 10;
+        for t in 0..thread_count {
+            let t_pool = conn_pool.clone();
+            let signaller = tx.clone();
+
+            std::thread::Builder::new()
+                .name(format!("test_multithreaded_read_write-{}", t))
+                .spawn(move || {
+                    for i in 0..10 {
+                        let id = format!("{}-{}", t, i);
+                        t_pool
+                            .execute_write::<_, (), InternalError>(|conn| {
+                                diesel::sql_query(
+                                    "INSERT INTO test_table (id, value) VALUES (?, ?)",
+                                )
+                                .bind::<diesel::sql_types::Text, _>(&id)
+                                .bind::<diesel::sql_types::Text, _>("test")
+                                .execute(conn)
+                                .map_err(|e| InternalError::from_source(Box::new(e)))?;
+
+                                Ok(())
+                            })
+                            .unwrap();
+
+                        let value = t_pool
+                            .execute_read::<_, _, InternalError>(|conn| {
+                                diesel::sql_query("SELECT * FROM test_table WHERE id = ?")
+                                    .bind::<diesel::sql_types::Text, _>(&id)
+                                    .get_results::<LookupData>(conn)
+                                    .map_err(|e| InternalError::from_source(Box::new(e)))
+                            })
+                            .unwrap();
+
+                        assert_eq!(id, value[0]._id);
+                        assert_eq!("test", &value[0]._value);
+                    }
+
+                    signaller.send(()).unwrap();
+                })
+                .unwrap();
+        }
+        drop(tx);
+
+        assert_eq!(rx.iter().count(), thread_count);
+
+        Ok(())
+    }
+
+    #[derive(QueryableByName)]
+    struct LookupData {
+        #[column_name = "id"]
+        #[sql_type = "diesel::sql_types::Text"]
+        _id: String,
+
+        #[column_name = "value"]
+        #[sql_type = "diesel::sql_types::Text"]
+        _value: String,
+    }
+
+    fn create_connection_pool(
+        test_name: &str,
+    ) -> Result<Pool<ConnectionManager<diesel::SqliteConnection>>, Box<dyn std::error::Error>> {
+        let connection_manager = ConnectionManager::<diesel::SqliteConnection>::new(&format!(
+            "file:{}?mode=memory&cache=shared",
+            test_name
+        ));
+        let pool = Pool::builder().build(connection_manager)?;
+
+        Ok(pool)
+    }
+}
