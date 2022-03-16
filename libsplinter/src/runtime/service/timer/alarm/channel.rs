2cd354aea1c638a9cfc3b89b7168ecc0a1776ceb
Andrea Gunderson <agunde@bitwise.io>
2022-03-16 09:20:37 -0500

    Add Timer for new service API
    The Timer is in charge of checking the configured
    `TimerFilters` for pending work.
    
    On start up the Timer takes a list of `TimerFilters`
    and their associated `TimerHandlerFactory`. The factories
    must return a handler that can handle messages of type `Vec<u8>`.
    
    On wake up, the Timer will check each `TimerFilter` for pending work.
    For each `FullyQualifiedServiceId` returned from the filter,
    the associated `TimerHandlerFactory` will be used to get a
    new `TimerHandler` that will run in a threadpool.
    
    A `Pacemaker` is used to wake up the Timer on some interval.
    An alarm is also provided to wake up the Timer prematurely.
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

---
+++ libsplinter/src/runtime/service/timer/alarm/channel.rs
@@ -0,0 +1,67 @@
+// Copyright 2018-2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! An alarm can be used to prematurely wake all or specific message handlers
+
+use std::sync::mpsc::Sender;
+
+use crate::error::InternalError;
+use crate::service::{FullyQualifiedServiceId, ServiceType};
+
+use super::{TimerAlarm, TimerMessage};
+
+pub struct ChannelTimerAlarm {
+    sender: Sender<TimerMessage>,
+}
+
+impl ChannelTimerAlarm {
+    pub fn new(sender: Sender<TimerMessage>) -> Self {
+        ChannelTimerAlarm { sender }
+    }
+}
+
+impl TimerAlarm for ChannelTimerAlarm {
+    /// Notify the `Timer` to check all `TimerFilters` for pending work
+    fn wake_up_all(&self) -> Result<(), InternalError> {
+        self.sender
+            .send(TimerMessage::WakeUpAll)
+            .map_err(|err| InternalError::from_source(Box::new(err)))
+    }
+
+    /// Notify the `Timer` to check a specific `TimerFilter` for pending work
+    ///
+    /// # Arguments
+    ///
+    /// * `service_type` - The service type of the the filter that will be checked
+    /// * `service_id` - An optional service ID
+    ///
+    /// If a service ID is provided, only the `TimerHandler` for that ID will be run. The serivce
+    /// ID must be returned from the `TimerFilter` to show there is pending work. If ther service
+    /// ID is not returned, no handlers will be run.
+    ///
+    /// If the service ID is not provided, the handlers for all service IDs returned from the
+    /// `TimerFilter` will be run.
+    fn wake_up(
+        &self,
+        service_type: ServiceType<'static>,
+        service_id: Option<FullyQualifiedServiceId>,
+    ) -> Result<(), InternalError> {
+        self.sender
+            .send(TimerMessage::WakeUp {
+                service_type,
+                service_id,
+            })
+            .map_err(|err| InternalError::from_source(Box::new(err)))
+    }
+}
