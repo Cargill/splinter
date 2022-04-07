ec242cb6b6e07d48a7741a742c5de6aa9614718e
Caleb Hill <hill@bitwise.io>
2022-04-07 18:41:55 -0500

    Change Method import to use full path
    Method is re-exported by the containing module however for code clarity I
    am using the full module path as this `Method` will have to coexist with
    other `Method`s from other modules and this change makes the type's
    provenance clearer.
    
    Signed-off-by: Caleb Hill <hill@bitwise.io>

--- libsplinter/src/rest_api/auth/authorization/permission_map.rs
+++ libsplinter/src/rest_api/auth/authorization/permission_map.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::rest_api::Method;
+use crate::rest_api::actix_web_1::Method;
 
 use super::Permission;
 
