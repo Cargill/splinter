a427298e09067c77781e886eb2ad296b699e11f4
Isabel Tomb <tomb@bitwise.io>
2022-04-14 18:58:39 -0500

    Add Action and Message structs to scabbard store
    Add Action and Message submodules to the scabbard_store module which
    contain structs used for storing consensus related information in the
    scabbard store.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

---
+++ services/scabbard/libscabbard/src/store/scabbard_store/two_phase/action.rs
@@ -0,0 +1,39 @@
+// Copyright 2018-2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::time::SystemTime;
+
+use splinter::service::ServiceId;
+
+use super::message::Scabbard2pcMessage;
+use crate::store::scabbard_store::context::ScabbardContext;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum ConsensusAction {
+    Update(ScabbardContext, Option<SystemTime>),
+    SendMessage(ServiceId, Scabbard2pcMessage),
+    Notify(ConsensusActionNotification),
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+pub enum ConsensusActionNotification {
+    Abort(),
+    Commit(),
+    MessageDropped(String),
+    RequestForStart(),
+    CoordinatorRequestForVote(),
+    ParticipantRequestForVote(Vec<u8>),
+}
