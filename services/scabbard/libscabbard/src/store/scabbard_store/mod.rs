543cef456b7f80ef43fbdd61b7f8a104c3696245
Isabel Tomb <tomb@bitwise.io>
2022-04-18 16:06:08 -0500

    Update `ScabbardStore` trait
    Update the `ScabbardStore` trait to include more store methods
    and add doc comments for all methods.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

--- services/scabbard/libscabbard/src/store/scabbard_store/mod.rs
+++ services/scabbard/libscabbard/src/store/scabbard_store/mod.rs
@@ -20,8 +20,11 @@ pub mod service;
 pub mod state;
 pub mod two_phase;
 
+use std::time::SystemTime;
+
 pub(crate) use error::ScabbardStoreError;
 
+use action::ScabbardConsensusAction;
 use commit::CommitEntry;
 use context::ScabbardContext;
 use service::ScabbardService;
@@ -29,28 +32,100 @@ use splinter::service::FullyQualifiedServiceId;
 
 pub trait ScabbardStore {
     /// Add a new context
+    ///
+    /// # Arguments
+    ///
+    /// * `service_id` - The combined `CircuitId` and `ServiceId` of the service the context
+    ///    belongs to
+    /// * `context` - The `ScabbardContext` to be added to the database
     fn add_consensus_context(
         &self,
         service_id: &FullyQualifiedServiceId,
         context: ScabbardContext,
     ) -> Result<(), ScabbardStoreError>;
     /// Update an existing context
+    ///
+    /// # Arguments
+    ///
+    /// * `service_id` - The combined `CircuitId` and `ServiceId` of the service the context
+    ///    belongs to
+    ///    context
+    /// * `context` - The `ScabbardContext` to be updated in the database
     fn update_consensus_context(
         &self,
         service_id: &FullyQualifiedServiceId,
         context: ScabbardContext,
     ) -> Result<(), ScabbardStoreError>;
+    /// Add a 2 phase commit coordinator action
+    ///
+    /// # Arguments
+    ///
+    /// * `action` - The `ScabbardConsensusAction` to be added to the database
+    /// * `service_id` - The combined `CircuitId` and `ServiceId` of the service the action
+    ///    belongs to
+    /// * `epoch` - The epoch that the given action belongs to
+    fn add_consensus_action(
+        &self,
+        action: ScabbardConsensusAction,
+        service_id: &FullyQualifiedServiceId,
+        epoch: u64,
+    ) -> Result<i64, ScabbardStoreError>;
+    /// Update an existing 2 phase commit action
+    ///
+    /// # Arguments
+    ///
+    /// * `service_id` - The combined `CircuitId` and `ServiceId` of the service the action
+    ///    belongs to
+    /// * `epoch` - The epoch that the action belongs to
+    /// * `action_id` - The ID of the action being updated
+    /// * `executed_at` - The time that the action was executed
+    fn update_consensus_action(
+        &self,
+        service_id: &FullyQualifiedServiceId,
+        epoch: u64,
+        action_id: i64,
+        executed_at: SystemTime,
+    ) -> Result<(), ScabbardStoreError>;
+    /// List all coordinator actions for a given service_id and epoch
+    ///
+    /// # Arguments
+    ///
+    /// * `service_id` - The combined `CircuitId` and `ServiceId` of the service for which actions
+    ///    should be listed
+    /// * `epoch` - The epoch for which actions should be listed
+    fn list_consensus_actions(
+        &self,
+        service_id: &FullyQualifiedServiceId,
+        epoch: u64,
+    ) -> Result<Vec<ScabbardConsensusAction>, ScabbardStoreError>;
     /// List ready services
     fn list_ready_services(&self) -> Result<Vec<FullyQualifiedServiceId>, ScabbardStoreError>;
     /// Add a new scabbard service
+    ///
+    /// # Arguments
+    ///
+    /// * `service` - The `ScabbardService` that is to be added to the database
     fn add_service(&self, service: ScabbardService) -> Result<(), ScabbardStoreError>;
     /// Add a new commit entry
+    ///
+    /// # Arguments
+    ///
+    /// * `commit_entry` - The `CommitEntry` that is to be added to the database
     fn add_commit_entry(&self, commit_entry: CommitEntry) -> Result<(), ScabbardStoreError>;
     /// Get the commit entry for the specified service_id and epoch
+    ///
+    /// # Arguments
+    ///
+    /// * `service_id` - The combined `CircuitId` and `ServiceId` of the service for which the last
+    ///    commit entry should be retrieved
     fn get_last_commit_entry(
         &self,
         service_id: &FullyQualifiedServiceId,
     ) -> Result<Option<CommitEntry>, ScabbardStoreError>;
     /// Update an existing commit entry
+    ///
+    /// # Arguments
+    ///
+    /// * `commit_entry` - The `CommitEntry` to be updated in the database
     fn update_commit_entry(&self, commit_entry: CommitEntry) -> Result<(), ScabbardStoreError>;
 }
