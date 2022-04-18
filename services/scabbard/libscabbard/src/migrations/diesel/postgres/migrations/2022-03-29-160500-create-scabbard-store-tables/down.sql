017348a01b1cd03d3ef3b64620f89ff2dfd080c0
Isabel Tomb <tomb@bitwise.io>
2022-04-18 16:31:36 -0500

    Add migrations for scabbard database tables
    Add the postgresql and sqlite migrations for the consensus and service
    commit history tables.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

---
+++ services/scabbard/libscabbard/src/migrations/diesel/postgres/migrations/2022-03-29-160500-create-scabbard-store-tables/down.sql
@@ -0,0 +1,28 @@
+-- Copyright 2018-2022 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS consensus_coordinator_context;
+DROP TABLE IF EXISTS consensus_coordinator_context_participant;
+DROP TABLE IF EXISTS consensus_action;
+DROP TABLE IF EXISTS consensus_update_coordinator_context_action;
+DROP TABLE IF EXISTS consensus_coordinator_send_message_action;
+DROP TABLE IF EXISTS consensus_coordinator_notification_action;
+DROP TABLE IF EXISTS consensus_update_coordinator_context_action_participant;
+DROP TABLE IF EXISTS consensus_participant_context;
+DROP TABLE IF EXISTS consensus_participant_context_participant;
+DROP TABLE IF EXISTS consensus_update_participant_context_action;
+DROP TABLE IF EXISTS consensus_update_participant_context_action_participant;
+DROP TABLE IF EXISTS consensus_participant_send_message_action;
+DROP TABLE IF EXISTS consensus_participant_notification_action;
---
+++ services/scabbard/libscabbard/src/migrations/diesel/postgres/migrations/2022-04-05-153027_scabbard_commit_history/up.sql
@@ -0,0 +1,38 @@
+-- Copyright 2018-2022 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+CREATE TYPE decision_type AS ENUM ('COMMIT', 'ABORT');
+CREATE TYPE scabbard_service_status_type AS ENUM ('PREPARED', 'FINALIZED', 'RETIRED');
+
+
+CREATE TABLE IF NOT EXISTS scabbard_service (
+    service_id       TEXT PRIMARY KEY NOT NULL,
+    status           scabbard_service_status_type NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS scabbard_peer (
+    service_id       TEXT NOT NULL,
+    peer_service_id  TEXT,
+    PRIMARY KEY(service_id, peer_service_id),
+    FOREIGN KEY(service_id) REFERENCES scabbard_service(service_id)
+);
+
+CREATE TABLE IF NOT EXISTS scabbard_v3_commit_history (
+    service_id TEXT NOT NULL,
+    epoch      INTEGER NOT NULL,
+    value      TEXT NOT NULL,
+    decision   decision_type,
+    PRIMARY KEY (service_id, epoch)
+);
