e1a80ecb142c8f8273d463029a3b173dfe763df7
Andrea Gunderson <agunde@bitwise.io>
2022-05-17 13:50:02 -0500

    Update ConsensusRunner for removal of Process from Algorithm
    Augrim has been updated to remove the generic.
    
    Signed-off-by: Andrea Gunderson <agunde@bitwise.io>

--- services/scabbard/libscabbard/src/service/v3/consensus/consensus_runner/mod.rs
+++ services/scabbard/libscabbard/src/service/v3/consensus/consensus_runner/mod.rs
@@ -30,7 +30,7 @@ use crate::store::action::ConsensusAction;
 use crate::store::context::ConsensusContext;
 use crate::store::event::ConsensusEvent;
 
-use super::{ConsensusActionRunner, ScabbardProcess};
+use super::ConsensusActionRunner;
 
 pub use action_source::UnprocessedActionSource;
 pub use builder::ConsensusRunnerBuilder;
@@ -53,7 +53,6 @@ where
         String,
         Box<
             dyn Algorithm<
-                ScabbardProcess,
                 Event = ConsensusEvent,
                 Action = ConsensusAction,
                 Context = ConsensusContext,
