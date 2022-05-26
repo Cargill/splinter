43cacbd1c650e9112d4544ff59b05420508b8287
Caleb Hill <hill@bitwise.io>
2022-05-26 12:11:08 -0500

    Split out OAuthConfig struct into its own file
    This commit moves the OAuthConfig struct into its own file to keep
    mod.rs clean.
    
    Signed-off-by: Caleb Hill <hill@bitwise.io>

--- libsplinter/src/rest_api/mod.rs
+++ libsplinter/src/rest_api/mod.rs
@@ -73,6 +73,8 @@ pub mod auth;
 #[cfg(feature = "rest-api-cors")]
 pub mod cors;
 mod errors;
+#[cfg(feature = "oauth")]
+mod oauth_config;
 pub mod paging;
 mod response_models;
 pub mod secrets;
@@ -80,15 +82,12 @@ pub mod sessions;
 
 use percent_encoding::{AsciiSet, CONTROLS};
 
-#[cfg(feature = "oauth")]
-use std::boxed::Box;
-
 #[cfg(all(feature = "oauth", feature = "rest-api-actix-web-1"))]
 use crate::oauth::rest_api::OAuthResourceProvider;
-#[cfg(feature = "oauth")]
-use crate::oauth::store::InflightOAuthRequestStore;
 
 pub use errors::{RequestError, RestApiServerError};
+#[cfg(feature = "oauth")]
+pub use oauth_config::OAuthConfig;
 
 pub use response_models::ErrorResponse;
 
@@ -146,60 +145,6 @@ impl std::fmt::Display for BindConfig {
             BindConfig::Http(bind) => write!(f, "{}", bind),
         }
     }
-}
-
-/// OAuth configurations that are supported out-of-the-box by the Splinter REST API.
-#[cfg(feature = "oauth")]
-pub enum OAuthConfig {
-    Azure {
-        /// The client ID of the Azure OAuth app
-        client_id: String,
-        /// The client secret of the Azure OAuth app
-        client_secret: String,
-        /// The redirect URL that is configured for the Azure OAuth app
-        redirect_url: String,
-        /// The URL of the OpenID discovery document for the Azure OAuth app
-        oauth_openid_url: String,
-        /// The store for in-flight requests
-        inflight_request_store: Box<dyn InflightOAuthRequestStore>,
-    },
-    /// OAuth provided by GitHub
-    GitHub {
-        /// The client ID of the GitHub OAuth app
-        client_id: String,
-        /// The client secret of the GitHub OAuth app
-        client_secret: String,
-        /// The redirect URL that is configured for the GitHub OAuth app
-        redirect_url: String,
-        /// The store for in-flight requests
-        inflight_request_store: Box<dyn InflightOAuthRequestStore>,
-    },
-    Google {
-        /// The client ID of the Google OAuth app
-        client_id: String,
-        /// The client secret of the Google OAuth app
-        client_secret: String,
-        /// The redirect URL that is configured for the Google OAuth app
-        redirect_url: String,
-        /// The store for in-flight requests
-        inflight_request_store: Box<dyn InflightOAuthRequestStore>,
-    },
-    OpenId {
-        /// The client ID of the OpenId OAuth app
-        client_id: String,
-        /// The client secret of the OpenId OAuth app
-        client_secret: String,
-        /// The redirect URL that is configured for the OpenId OAuth app
-        redirect_url: String,
-        /// The URL of the OpenID discovery document for the OpenId OAuth app
-        oauth_openid_url: String,
-        /// Additional parameters to add to auth requests made to the OpenID OAuth provider
-        auth_params: Option<Vec<(String, String)>>,
-        /// Additional scopes to request from the OpenID OAuth provider
-        scopes: Option<Vec<String>>,
-        /// The store for in-flight requests
-        inflight_request_store: Box<dyn InflightOAuthRequestStore>,
-    },
 }
 
 pub fn percent_encode_filter_query(input: &str) -> String {
