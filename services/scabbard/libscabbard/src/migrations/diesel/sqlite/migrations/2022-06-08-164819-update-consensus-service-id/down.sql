942372e6be6d057edbcf27ae7aab68e98765eb56
Isabel Tomb <tomb@bitwise.io>
2022-06-08 18:19:56 -0500

    Add migrations to split service_id
    Add migrations that split the service_id column in the 2pc consensus
    tables into circuit_id and service_id.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

---
+++ services/scabbard/libscabbard/src/migrations/diesel/sqlite/migrations/2022-06-08-164819-update-consensus-service-id/down.sql
@@ -0,0 +1,107 @@
+-- Copyright 2018-2022 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+-- Rename the updated tables
+ALTER TABLE consensus_2pc_context RENAME TO old_consensus_2pc_context;
+ALTER TABLE consensus_2pc_context_participant RENAME TO old_consensus_2pc_context_participant;
+ALTER TABLE consensus_2pc_event RENAME TO old_consensus_2pc_event;
+
+-- Recreate the tables without the circuit_id columns
+CREATE TABLE IF NOT EXISTS consensus_2pc_context (
+    service_id                TEXT PRIMARY KEY,
+    coordinator               TEXT NOT NULL,
+    epoch                     BIGINT NOT NULL,
+    last_commit_epoch         BIGINT,
+    state                     TEXT NOT NULL
+    CHECK ( state IN ( 'WAITINGFORSTART', 'VOTING', 'WAITINGFORVOTE', 'ABORT', 'COMMIT', 'WAITINGFORVOTEREQUEST', 'VOTED') ),
+    vote_timeout_start        BIGINT
+    CHECK ( (vote_timeout_start IS NOT NULL) OR ( state != 'VOTING') ),
+    vote                      TEXT
+    CHECK ( (vote IN ('TRUE' , 'FALSE')) OR ( state != 'VOTED') ),
+    decision_timeout_start    BIGINT
+    CHECK ( (decision_timeout_start IS NOT NULL) OR ( state != 'VOTED') )
+);
+
+CREATE TABLE IF NOT EXISTS consensus_2pc_context_participant (
+    service_id                TEXT NOT NULL,
+    epoch                     BIGINT NOT NULL,
+    process                   TEXT NOT NULL,
+    vote                      TEXT
+    CHECK ( vote IN ('TRUE' , 'FALSE') OR vote IS NULL ),
+    PRIMARY KEY (service_id, process),
+    FOREIGN KEY (service_id) REFERENCES consensus_2pc_context(service_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS consensus_2pc_event (
+    id                        INTEGER PRIMARY KEY AUTOINCREMENT,
+    service_id                TEXT NOT NULL,
+    created_at                TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    executed_at               BIGINT,
+    event_type                TEXT NOT NULL
+    CHECK ( event_type IN ('ALARM', 'DELIVER', 'START', 'VOTE') )
+);
+
+-- Move data from the old tables into the updated tables
+INSERT INTO consensus_2pc_context
+    (
+        service_id,
+        coordinator,
+        epoch,
+        last_commit_epoch,
+        state,
+        vote_timeout_start,
+        vote,
+        decision_timeout_start
+    )
+    SELECT
+        circuit_id || '::' || service_id,
+        coordinator,
+        epoch,
+        last_commit_epoch,
+        state,
+        vote_timeout_start,
+        vote,
+        decision_timeout_start
+    FROM old_consensus_2pc_context;
+
+INSERT INTO consensus_2pc_context_participant
+    (
+        service_id,
+        epoch,
+        process,
+        vote
+    )
+    SELECT
+        circuit_id || '::' || service_id,
+        epoch,
+        process,
+        vote
+    FROM old_consensus_2pc_context_participant;
+
+INSERT INTO consensus_2pc_event
+    (
+        id,
+        service_id,
+        executed_at,
+        created_at,
+        event_type
+    )
+    SELECT
+        id,
+        circuit_id || '::' || service_id,
+        executed_at,
+        created_at,
+        event_type
+    FROM old_consensus_2pc_event;
---
+++ services/scabbard/libscabbard/src/migrations/diesel/sqlite/migrations/2022-06-08-164819-update-consensus-service-id/up.sql
@@ -0,0 +1,116 @@
+-- Copyright 2018-2022 Cargill Incorporated
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- -----------------------------------------------------------------------------
+
+-- Rename the existing tables
+ALTER TABLE consensus_2pc_context RENAME TO old_consensus_2pc_context;
+ALTER TABLE consensus_2pc_context_participant RENAME TO old_consensus_2pc_context_participant;
+ALTER TABLE consensus_2pc_event RENAME TO old_consensus_2pc_event;
+
+-- Recreate the tables with the circuit_id column
+CREATE TABLE IF NOT EXISTS consensus_2pc_context (
+    circuit_id                TEXT NOT NULL,
+    service_id                TEXT NOT NULL,
+    coordinator               TEXT NOT NULL,
+    epoch                     BIGINT NOT NULL,
+    last_commit_epoch         BIGINT,
+    state                     context_state NOT NULL,
+    vote_timeout_start        BIGINT
+    CHECK ( (vote_timeout_start IS NOT NULL) OR ( state != 'VOTING') ),
+    vote                      TEXT
+    CHECK ( (vote IN ('TRUE' , 'FALSE')) OR ( state != 'VOTED') ),
+    decision_timeout_start    BIGINT
+    CHECK ( (decision_timeout_start IS NOT NULL) OR ( state != 'VOTED') ),
+    PRIMARY KEY (circuit_id, service_id)
+);
+
+CREATE TABLE IF NOT EXISTS consensus_2pc_context_participant (
+    circuit_id                TEXT NOT NULL,
+    service_id                TEXT NOT NULL,
+    epoch                     BIGINT NOT NULL,
+    process                   TEXT NOT NULL,
+    vote                      TEXT
+    CHECK ( vote IN ('TRUE' , 'FALSE') OR vote IS NULL ),
+    PRIMARY KEY (circuit_id, service_id, process)
+);
+
+CREATE TABLE IF NOT EXISTS consensus_2pc_event (
+    id                        BIGSERIAL PRIMARY KEY,
+    circuit_id                TEXT NOT NULL,
+    service_id                TEXT NOT NULL,
+    created_at                TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    executed_at               BIGINT,
+    event_type                event_type NOT NULL
+);
+
+-- Move data from the old tables into the updated tables, splitting service_id into its two parts
+-- circuit_id and service_id
+INSERT INTO consensus_2pc_context
+    (
+        circuit_id,
+        service_id,
+        coordinator,
+        epoch,
+        last_commit_epoch,
+        state,
+        vote_timeout_start,
+        vote,
+        decision_timeout_start
+    )
+    SELECT
+        substr(service_id, 1, instr(service_id, '::')-1) AS circuit_id,
+        substr(service_id, instr(service_id, '::')+2) AS service_id,
+        coordinator,
+        epoch,
+        last_commit_epoch,
+        state,
+        vote_timeout_start,
+        vote,
+        decision_timeout_start
+    FROM old_consensus_2pc_context;
+
+INSERT INTO consensus_2pc_context_participant
+    (
+        circuit_id,
+        service_id,
+        epoch,
+        process,
+        vote
+    )
+    SELECT
+        substr(service_id, 1, instr(service_id, '::')-1) AS circuit_id,
+        substr(service_id, instr(service_id, '::')+2) AS service_id,
+        epoch,
+        process,
+        vote
+    FROM old_consensus_2pc_context_participant;
+
+INSERT INTO consensus_2pc_event
+    (
+        id,
+        circuit_id,
+        service_id,
+        created_at,
+        executed_at,
+        event_type
+    )
+    SELECT
+        id,
+        substr(service_id, 1, instr(service_id, '::')-1) AS circuit_id,
+        substr(service_id, instr(service_id, '::')+2) AS service_id,
+        created_at,
+        executed_at,
+        event_type
+    FROM old_consensus_2pc_event;
+    
