a552cfb2e7168383970b7b500cf77789a814800e
Isabel Tomb <tomb@bitwise.io>
2022-07-15 15:57:55 -0500

    Add migrations for the echo service tables
    Add a migrations module containing the postgresql and sqlite migrations
    for the tables that will be used in the echo service.
    
    Signed-off-by: Isabel Tomb <tomb@bitwise.io>

---
+++ services/echo/libecho/src/lib.rs
@@ -0,0 +1,23 @@
+// Copyright 2018-2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[cfg(feature = "diesel_migrations")]
+pub mod migrations;
+
+#[macro_use]
+#[cfg(any(feature = "sqlite", feature = "postgres"))]
+extern crate diesel;
+#[cfg(feature = "diesel_migrations")]
+#[macro_use]
+extern crate diesel_migrations;
---
+++ services/echo/libecho/src/migrations/diesel/sqlite/mod.rs
@@ -0,0 +1,33 @@
+// Copyright 2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Tools to apply database migrations for SQLite.
+
+embed_migrations!("./src/migrations/diesel/sqlite/migrations");
+
+use diesel::sqlite::SqliteConnection;
+
+use splinter::error::InternalError;
+
+/// Run all pending database migrations.
+///
+/// # Arguments
+///
+/// * `conn` - Connection to SQLite database
+///
+pub fn run_migrations(conn: &SqliteConnection) -> Result<(), InternalError> {
+    embedded_migrations::run(conn).map_err(|err| InternalError::from_source(Box::new(err)))?;
+
+    Ok(())
+}
